ac696035177b06934d24762eab0d0ee4daf6ea16
marcel-krsh <marcel.krsh@gmail.com>
2018-07-17 22:42:20 -0400

    Shift 5.6 Pull Request Fixes

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace Tests;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
@@ -13,7 +15,11 @@ trait CreatesApplication
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
+
         $app->make(Kernel::class)->bootstrap();
+
+        Hash::driver('bcrypt')->setRounds(4);
+
         return $app;
     }
 }
