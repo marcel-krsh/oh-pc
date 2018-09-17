7337a40535fa5af119d8dffd5c3dc20a18fd5921
marcel-krsh <marcel.krsh@gmail.com>
2018-09-17 11:37:46 -0400

    API docuware

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -30,63 +30,90 @@ class DocumentService extends PCAPIService
     }
     
     /**
-     * @param int $amount
+     * Search Documents
+     * 
+     * @param array $search 
+     * @param int $user
+     * @param string $user_email
+     * @param string $user_name
+     * @param int $device_id
+     * @param string $device_name
+     * @param string $provider
      */
-    public function getDocuments($amount=100)
+    public function getDocuments(array $search=null, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware')
     {
-        return $this->get("docuware/documents/search?amount={$amount}");
-    }
+        // example call
+        // /api/v1/docuware/documents/search?search=FIELD:criteria;FIELD1:fromcriteria,toCriteria;&page={{null/value}}&user={{user_id}}&user_email={{user_email}}&user_name={{user_full_name}}&device_id={{device_id}}&device_name={{device_name}}
 
-    /**
-     * Get Recent Documents
-     *
-     * @param null $last_updated_at
-     */
-    public function getRecentDocuments($last_updated_at=null)
-    {
-        if(!$last_updated_at) {
-            $last_updated_at = Carbon::now()->addMonth(-1);
+        // search array reference
+        // $search = [
+        //     'cabinet' => null,
+        //     'fields' => [
+        //         [
+        //             'field' => 'field1',
+        //             'criteria' => 'criteria',
+        //             'criteria2' => null
+        //         ],
+        //         [
+        //             'field' => 'field1',
+        //             'criteria' => 'from criteria',
+        //             'criteria2' => 'to criteria'
+        //         ]
+        //     ],
+        //     'isandoperation' => 'false'
+        // ];
+
+        if($search !== null){
+            (isset($search['cabinet']) || array_key_exists('cabinet', $search)) ? $cabinet = $search['cabinet'] : $cabinet = '';
+            (isset($search['isandoperation']) || array_key_exists('isandoperation', $search)) ? $isandoperation = $search['isandoperation'] : $isandoperation = ''; // true, false or nothing
+
+            if(isset($search['fields']) || array_key_exists('fields', $search)){
+                $search_fields = "search=";
+                foreach($search['fields'] as $field){
+                    if((isset($field['field']) || array_key_exists('field', $field)) && (isset($field['criteria']) || array_key_exists('criteria', $field))){
+
+                        $search_fields = $search_fields."{$field['field']}:{$field['criteria']}";
+
+                        if(((isset($field['criteria2']) || array_key_exists('criteria2', $field))){
+                            $search_fields = $search_fields.",{$field['criteria2']}";
+                        }
+
+                        $search_fields = $search_fields.";";
+                    }
+                }
+                $search_params = "{$search_fields}&cabinet={$cabinet}&isandoperation={$isandoperation}&";
+            }else{
+                $search_params = "cabinet={$cabinet}&isandoperation={$isandoperation}&";
+            }
+
+        }else{
+            $search_params =  "";
         }
 
-        $project_id = 0;
-        $low_date = (new Carbon($last_updated_at))->format('n/j/Y g:i:s A');
-        $high_date = Carbon::now()->addDay(1)->format('n/j/Y g:i:s A');
-        $search_params = "DWMODDATETIME:{$low_date},{$high_date};&isandoperation=true;";
+        $log_params = "user={$user_id}&user_email={$user_email}&user_name={$user_full_name}&device_id={$device_id}&device_name={$device_name}";
 
-        return $this->get("docuware/documents/search?search={$search_params}");
+        return $this->get("{$provider}/documents/search?{$search_params}{$log_params}");
     }
 
     /**
-     * Get Recent Documents by Project ID
-     *
-     * @param int $project_id
-     * @param null $last_updated_at
+     * Get Specific Document
+     * 
+     * @param int $id
+     * @param int $user
+     * @param string $user_email
+     * @param string $user_name
+     * @param int $device_id
+     * @param string $device_name
+     * @param string $provider
      */
-    public function getRecentDocumentsByProjectId($project_id, $last_updated_at=null)
+    public function getDocument(int $id, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware') : string
     {
-        if(!$last_updated_at) {
-            $last_updated_at = Carbon::now()->addMonth(-1);
-        }
-
-        $project_id = 0;
-        $low_date = (new Carbon($last_updated_at))->format('n/j/Y g:i:s A');
-        $high_date = Carbon::now()->addDay(1)->format('n/j/Y g:i:s A');
-        $search_params = "PROJECTNUMBER:{$project_id};DWMODDATETIME:{$low_date},{$high_date};&isandoperation=true;";
-
-        return $this->get("docuware/documents/search?search={$search_params}");
-    }
-
-    /**
-     * Get Document By Project ID
-     *
-     * @param int $project_id
-     */
-    public function getDocumentsByProjectId($project_id)
-    {
-        $project_id = 0;
-        $search_params = "PROJECTNUMBER:{$project_id};&isandoperation=true;";
-
-        return $this->get("docuware/documents/search?search={$search_params}");
+        // example call
+        // /api/v1/docuware/document/{{document_id}}?user={{user_id}}&user_email={{user_email}}&user_name={{user_full_name}}&device_id={{device_id}}&device_name={{device_name}}
+        
+        $log_params = "user={$user_id}&user_email={$user_email}&user_name={$user_full_name}&device_id={$device_id}&device_name={$device_name}";
+        
+        return $this->get("{$provider}/document/{$id}?{$log_params}");
     }
 
     /**
@@ -95,37 +122,234 @@ class DocumentService extends PCAPIService
      * ie. Docuware ID
      *
      * @param $provider_id
+     * @param int $user
+     * @param string $user_email
+     * @param string $user_name
+     * @param int $device_id
+     * @param string $device_name
+     * @param string $provider
      */
-    public function getDocumentByProviderId($provider_id)
+    public function getDocumentByProviderId(int $provider_id, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware') : string
     {
-        $project_id = 0;
-        $search_params = "doc-id:{$provider_id};&isandoperation=true;";
+        $search = [
+            'fields' => [
+                [
+                    'field' => 'doc-id',
+                    'criteria' => $provider_id
+                ]
+            ],
+            'isandoperation' => 'true'];
 
-        return $this->get("docuware/documents/search?search={$search_params}");
+        return $this->getDocuments($search, $user, $user_email, $user_name, $device_id, $device_name, $provider);
+    }
+
+    /**
+     * Get Recent Documents
+     *
+     * @param $last_updated_at Carbon date
+     * @param int $user
+     * @param string $user_email
+     * @param string $user_name
+     * @param int $device_id
+     * @param string $device_name
+     * @param string $provider
+     */
+    public function getRecentDocuments(string $last_updated_at, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware')
+    {
+        if(!$last_updated_at) {
+            $last_updated_at = Carbon::now()->addMonth(-1);
+        }
+
+        $low_date = (new Carbon($last_updated_at))->format('n/j/Y g:i:s A');
+        $high_date = Carbon::now()->addDay(1)->format('n/j/Y g:i:s A');
+        $search = [
+            'fields' => [
+                [
+                    'field' => 'DWMODDATETIME',
+                    'criteria' => $low_date,
+                    'criteria2' => $high_date
+                ]
+            ],
+            'isandoperation' => 'true'];
+
+        return $this->getDocuments($search, $user, $user_email, $user_name, $device_id, $device_name, $provider);
+    }
+
+    /**
+     * Get Recent Documents by Project ID
+     *
+     * @param int $project_id
+     * @param $last_updated_at Carbon date
+     * @param int $user
+     * @param string $user_email
+     * @param string $user_name
+     * @param int $device_id
+     * @param string $device_name
+     * @param string $provider
+     */
+    public function getRecentDocumentsByProjectId(int $project_id, string $last_updated_at , int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware')
+    {
+        if(!$last_updated_at) {
+            $last_updated_at = Carbon::now()->addMonth(-1);
+        }
+
+        $project_id = 0;
+        $low_date = (new Carbon($last_updated_at))->format('n/j/Y g:i:s A');
+        $high_date = Carbon::now()->addDay(1)->format('n/j/Y g:i:s A');
+
+        $search = [
+            'fields' => [
+                [
+                    'field' => 'PROJECTNUMBER',
+                    'criteria' => $project_id
+                ],
+                [
+                    'field' => 'DWMODDATETIME',
+                    'criteria' => $low_date,
+                    'criteria2' => $high_date
+                ]
+            ],
+            'isandoperation' => 'true'
+        ];
+
+        return $this->getDocuments($search, $user, $user_email, $user_name, $device_id, $device_name, $provider);
+    }
+
+    /**
+     * Get Documents By Project ID
+     *
+     * @param int $project_id
+     * @param int $user
+     * @param string $user_email
+     * @param string $user_name
+     * @param int $device_id
+     * @param string $device_name
+     * @param string $provider
+     */
+    public function getDocumentsByProjectId(int $project_id, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware')
+    {
+        $search = [
+            'fields' => [
+                [
+                    'field' => 'PROJECTNUMBER',
+                    'criteria' => $project_id
+                ]
+            ],
+            'isandoperation' => 'true'
+        ];
+
+        return $this->getDocuments($search, $user, $user_email, $user_name, $device_id, $device_name, $provider);
     }
 
     /**
      * Update Document Metadata
      *
-     * @param $provider_id
-     * @param $metadata
+     * @param int $id
+     * @param array $metadata
+     * @param int $user
+     * @param string $user_email
+     * @param string $user_name
+     * @param int $device_id
+     * @param string $device_name
+     * @param string $provider
+     * @return bool
      */
-    public function updateDocumentMetadata($provider_id, $metadata)
+    public function updateDocumentMetadata(int $id, array $metadata, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware') : bool
     {
-        $payload = '';
+        //  $metadata = [
+        //     'field_name' => 'value',
+        //     'field_name' => 'value',
+        //     'field_name' => 'value'
+        //  ];
 
+        $log_params = "user={$user_id}&user_email={$user_email}&user_name={$user_full_name}&device_id={$device_id}&device_name={$device_name}";
 
-        return $this->put("docuware/documents/{$provider_id}", $payload);
+        return $this->put("{$provider}/document/{$id}?{$log_params}", $metadata);
     }
 
     /**
-     * Delete Document By Provider ID
-     *
-     * @param $provider_id
+     * 
+     * Delete Document by ID or by search query
+     * 
+     * @param  int $id The id of the document to delete
+     * @param  int $user
+     * @param  string $user_email
+     * @param  string $user_name
+     * @param  int $device_id
+     * @param  string $device_name
+     * @param  string $provider The document provider's name
+     * @return bool
      */
-    public function deleteDocumentByProviderId($provider_id)
+    public function deleteDocument(int $id, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware') : bool
     {
-        return $this->delete("docuware/documents/{$provider_id}");
+        // example call
+        // /api/v1/docuware/document/{{document_id}}?user={{user_id}}&user_email={{user_email}}&user_name={{user_full_name}}&device_id={{device_id}}&device_name={{device_name}}
+
+        $log_params = "user={$user_id}&user_email={$user_email}&user_name={$user_full_name}&device_id={$device_id}&device_name={$device_name}";
+
+        return $this->delete("{$provider}/document/{$id}?{$log_params}");
+    }
+
+    /**
+     * Chain of Custody - adds a log in Docuware that the document was deleted from device
+     *
+     * @param  int $id Document ID
+     * @param  int $user
+     * @param  string $user_email
+     * @param  string $user_name
+     * @param  int $device_id
+     * @param  string $device_name
+     * @param  string $provider The document provider's name
+     * 
+     * @return bool
+     */
+    public function chainOfCustodyDeleteFromDevice(int $id, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware') : bool 
+    {
+        $log_params = [
+            'user' => $user,
+            'user_email' => $user_email,
+            'user_name' => $user_name,
+            'device_id' => $device_id,
+            'device_name' => $device_name,
+        ];
+
+        $payload = json_encode($log_params);
+
+        return $this->post("{$provider}/document/{$id}/deleted", $payload);
+    }
+
+    /**
+     * Store Document
+     * 
+     * @param  array $metadata Document's fields and values
+     * @param  $user
+     * @param  string $user_email
+     * @param  string $user_name
+     * @param  int $device_id
+     * @param  string $device_name
+     * @param  string $provider The document provider's name
+     * 
+     * @return int Document ID
+     */
+    public function storeDocument(array $metadata, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, string $provider = 'docuware') : int{
+
+        //  $metadata = [
+        //     'field_name' => 'value',
+        //     'field_name' => 'value',
+        //     'field_name' => 'value'
+        //  ];
+
+        $log_params = [
+            'user' => $user,
+            'user_email' => $user_email,
+            'user_name' => $user_name,
+            'device_id' => $device_id,
+            'device_name' => $device_name,
+        ];
+
+        $payload = array_merge($metadata,$log_params);
+
+        return $this->post("{$provider}/document", $payload);
     }
 
 }
