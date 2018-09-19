af156b82b60f49ac25e98335843e8fc7bc11ed97
marcel-krsh <marcel.krsh@gmail.com>
2018-09-19 02:10:45 -0400

    test

--- app/Http/Middleware/AllitaAuth.php
+++ app/Http/Middleware/AllitaAuth.php
@@ -97,7 +97,7 @@ class AllitaAuth
             // check credentials with Devco
             $check_credentials = $this->_auth_service->userAuthenticateToken($request->get('token'), $ip, $user_agent);
 
-            dd($check_credentials->data->attributes->authentication-message);
+            dd($check_credentials->data->attributes->{'authentication-message'});
 
             // dd($devco_auth->rootAuthenticate());
             // dd($devco_auth->getLoginUrl());
