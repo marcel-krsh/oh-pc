d47b08ef78b38b216e8b9d98c046d67d720de573
marcel-krsh <marcel.krsh@gmail.com>
2018-09-18 20:39:56 -0400

    Remove DD()

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -107,7 +107,7 @@ class AuthService
             $response = $this->_client->request('GET', $endpoint);
             if ($response->getStatusCode() === 200) {
                 $result = json_decode($response->getBody()->getContents());
-                dd($result);
+                //dd($result);
                 $this->_updateAccessToken($result->access_token);
                 $this->_updateRefreshtoken($result->refresh_token);
                 $is_successful = true;
