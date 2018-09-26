6586c0e2963d03217849a4fc4ae212a983d1bf6d
marcel-krsh <marcel.krsh@gmail.com>
2018-09-26 15:01:08 -0400

    checking the failed login logging

--- app/Http/Middleware/AllitaAuth.php
+++ app/Http/Middleware/AllitaAuth.php
@@ -187,7 +187,7 @@ class AllitaAuth
         ){
             // credentials passed through the get string - let us validate with DevCo
             $checkCredentials = $this->_auth_service->userAuthenticateToken($passedCredentials['user_id'], $passedCredentials['token'], $passedIp, $passedUserAgent);
-                dd($checkCredentials);
+                //dd($checkCredentials);
                 if($checkCredentials == false || !$checkCredentials->data->attributes->{'authenticated'} || !$checkCredentials->data->attributes->{'user-activated'} || !$checkCredentials->data->attributes->{'user-exists'}){
                     // this is a failed login attempt
                     $failedLoginAttempt = true;
@@ -257,6 +257,7 @@ class AllitaAuth
         ///
 
         if($failedLoginAttempt){
+            dd('got inside... failed login')
             if(is_null($currentlyBlocked)) {
                 // there is not a tracker for this yet - insert one:
                 $newTracker = new AuthTracker([
