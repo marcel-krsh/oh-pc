68c8640bd52efee3d9875eb9a580ae899be3c70b
marcel-krsh <marcel.krsh@gmail.com>
2018-09-26 14:58:40 -0400

    testing

--- app/Http/Middleware/AllitaAuth.php
+++ app/Http/Middleware/AllitaAuth.php
@@ -187,7 +187,7 @@ class AllitaAuth
         ){
             // credentials passed through the get string - let us validate with DevCo
             $checkCredentials = $this->_auth_service->userAuthenticateToken($passedCredentials['user_id'], $passedCredentials['token'], $passedIp, $passedUserAgent);
-
+                dd($checkCredentials);
                 if($checkCredentials == false || !$checkCredentials->data->attributes->{'authenticated'} || !$checkCredentials->data->attributes->{'user-activated'} || !$checkCredentials->data->attributes->{'user-exists'}){
                     // this is a failed login attempt
                     $failedLoginAttempt = true;
