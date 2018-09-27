4e8c9d357314435e700f8a2c342eb2146dda7aa2
marcel-krsh <marcel.krsh@gmail.com>
2018-09-27 11:41:57 -0400

    check if it is a string

--- app/Http/Middleware/AllitaAuth.php
+++ app/Http/Middleware/AllitaAuth.php
@@ -195,7 +195,7 @@ class AllitaAuth
             // credentials passed through the get string - let us validate with DevCo
             $checkCredentials = $this->_auth_service->userAuthenticateToken($passedCredentials['user_id'], $passedCredentials['token'], $passedIp, $passedUserAgent);
                 //dd($checkCredentials);
-                if(!$checkCredentials->data->attributes->{'authenticated'} || !$checkCredentials->data->attributes->{'user-activated'} || !$checkCredentials->data->attributes->{'user-exists'}){
+                if(is_string($checkCredentials) || !$checkCredentials->data->attributes->{'authenticated'} || !$checkCredentials->data->attributes->{'user-activated'} || !$checkCredentials->data->attributes->{'user-exists'}){
                     // this is a failed login attempt
                     $failedLoginAttempt = true;
                     $failedLoginReason = $checkCredentials;
