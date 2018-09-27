cf5d728e53528439dcaac481286b982084c6727f
marcel-krsh <marcel.krsh@gmail.com>
2018-09-27 10:25:56 -0400

    Fix js bug

--- app/Http/Middleware/AllitaAuth.php
+++ app/Http/Middleware/AllitaAuth.php
@@ -405,7 +405,7 @@ class AllitaAuth
         // user false // not logged in and/or no credentials
         if($user == false){
             //dd('User login failed: '.$failedLoginReason);
-            exit('<html><head></head><body><script>alert(\'Uh oh, looks like your login expired. I am taking you to DevCo to get you logged in.\'); window.location=\''.$devcoLoginUrl.'\');</script></body></html>');
+            exit('<script>alert(\'Uh oh, looks like your login expired. I am taking you to DevCo to get you logged in.\'); window.location=\''.$devcoLoginUrl.'\';</script>');
             
         }
 
