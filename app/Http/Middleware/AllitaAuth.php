684d85f651cf4da8ac82d364b82aecbe0506b7b2
marcel-krsh <marcel.krsh@gmail.com>
2018-09-27 12:00:05 -0400

    Use it

--- app/Http/Middleware/AllitaAuth.php
+++ app/Http/Middleware/AllitaAuth.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Cookie;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Contracts\Auth\Guard;
