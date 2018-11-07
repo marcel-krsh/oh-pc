c57f0afa94c72e3d148820fb877fa3054924b90c
marcel-krsh <marcel.krsh@gmail.com>
2018-11-06 22:50:48 -0500

    Add Use

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Log;
+use app\Jobs\SyncDevco;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
