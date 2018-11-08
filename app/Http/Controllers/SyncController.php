7d07600b22e201fc510410a15015423b5e319d6b
marcel-krsh <marcel.krsh@gmail.com>
2018-11-08 00:05:00 -0500

    Json Decode

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -16,7 +16,7 @@ class SyncController extends Controller
     //
     public function sync() {
     	$test = new DevcoService();
-    	return $test->listAddresses(1, 'january 1, 2010', 1,'brian@allita.org', 'Brian Greenwood', 1, 'Server');
+    	return json_decode($test->listAddresses(1, 'january 1, 2010', 1,'brian@allita.org', 'Brian Greenwood', 1, 'Server'));
 
 		
     }
