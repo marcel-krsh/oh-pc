191384f521295d832a9caa818c734a49e65274e4
marcel-krsh <marcel.krsh@gmail.com>
2018-11-08 00:33:44 -0500

    Trying new date

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -17,7 +17,10 @@ class SyncController extends Controller
     public function sync() {
     	$test = new DevcoService();
     	$addresses = $test->listAddresses(1, '2019-04-23T08:50:19.637', 1,'brian@allita.org', 'Brian Greenwood', 1, 'Server');
-    	return $addresses;
+    	forEach($addresses[0] as $address){
+    		$output .= $addresses['id']."<br />";
+    	}
+    	return $output;
 
 		
     }
