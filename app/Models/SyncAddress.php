dc03dbbd2a67f57454ba14a0aad104ae78f6016c
marcel-krsh <marcel.krsh@gmail.com>
2018-11-20 16:33:15 -0500

    Add fillable to model

--- app/Models/SyncAddress.php
+++ app/Models/SyncAddress.php
@@ -8,4 +8,20 @@ class SyncAddress extends Model
 {
 	public $timestamps = true;
     //
+    protected $fillable = [
+        'devco_id',
+        'allita_id',
+        'line_1',
+        'line_2',
+        'city',
+        'state_id',
+        'state',
+        'zip',
+        'zip_4',
+        'longitude',
+        'latitude',
+        'last_edited',
+        'created_at',
+        'updated_at'
+    ];
 }
