648d5a27c131fd09c5c3f1db25e0cec840951314
marcel-krsh <marcel.krsh@gmail.com>
2018-11-20 15:15:48 -0500

    Start Putting It into a DB

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -4,27 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// API Specific
 use App\Services\AuthService;
 use App\Services\DevcoService;
 use App\Models\AuthTracker;
 use App\Models\SystemSetting;
 use App\Models\User;
-use App\Models\SyncAddress;
 use Illuminate\Support\Facades\Hash;
+
+// SYNC MODLES
+use App\Models\SyncAddress;
+use App\Models\Address;
+
+
 
 class SyncController extends Controller
 {
     //
     public function sync() {
-        /// Addresses sync
+        //////////////////////////////////////////////////
+        /////// Address Sync
+        /////
 
         /// get last modified date inside the database
         $lastModifiedDate = SyncAddress::select('updated_at')->orderBy('updated_at','desc')->first();
         // if the value is null set a default start date to start the sync.
         if(is_null($lastModifiedDate)) {
-            $modified = '10/1/2017';
+            $modified = '10/1/1900';
         }else{
-            $modified = $lastModifiedDate->updated_at;
+            $modified = $lastModifiedDate->last_edited;
         }
     	$apiConnect = new DevcoService();
         if(!is_null($apiConnect)){
@@ -36,7 +44,40 @@ class SyncController extends Controller
                 echo "<hr />PAGE ".$syncPage."<hr />";
                 foreach($syncData['data'] as $i => $v)
                     {
-                        echo $v['id'].' '.$v['attributes']['line1'].' '.' '.$v['attributes']['city'].' '.$v['attributes']['state'].' '.$v['attributes']['zipCode'].' '.$v['attributes']['zip4'].' '.$v['attributes']['latitude'].' '.$v['attributes']['longitude'].' '.$v['attributes']['addressKey'].' '.$v['attributes']['lastEdited'].'<br/>';
+                        // check if record exists
+                        $updateRecord = Address::select('id')->where('devco_id',$v['id'])->first();
+
+                        if(isset($updateRecord->id)) {
+                            // record exists - update it.
+                            Address::where('id',$updateRecord['id'])
+                            ->update([
+                                'line_1'=>$v['attributes']['line1'],
+                                'line_2'=>$v['attributes']['line2'],
+                                'city'=>$v['attributes']['city'],
+                                'state'=>$v['attributes']['state'],
+                                'zip'=>$v['attributes']['zipCode'],
+                                'zip_4'=>$v['attributes']['zip4'],
+                                'longitude'=>$v['attributes']['latitude'],
+                                'latitude'=>$v['attributes']['longitude'],
+                                'address_key'=>$v['attributes']['addressKey'],
+                                'last_edited'=>$v['attributes']['lastEdited'],
+                            ]);
+                        } else {
+                            Address::insert([
+                                'line_1'=>$v['attributes']['line1'],
+                                'line_2'=>$v['attributes']['line2'],
+                                'city'=>$v['attributes']['city'],
+                                'state'=>$v['attributes']['state'],
+                                'zip'=>$v['attributes']['zipCode'],
+                                'zip_4'=>$v['attributes']['zip4'],
+                                'longitude'=>$v['attributes']['latitude'],
+                                'latitude'=>$v['attributes']['longitude'],
+                                'address_key'=>$v['attributes']['addressKey'],
+                                'last_edited'=>$v['attributes']['lastEdited'],
+                            ]);
+                        }
+
+                        //echo $v['id'].' '.$v['attributes']['line1'].' '.' '.$v['attributes']['city'].' '.$v['attributes']['state'].' '.$v['attributes']['zipCode'].' '.$v['attributes']['zip4'].' '.$v['attributes']['latitude'].' '.$v['attributes']['longitude'].' '.$v['attributes']['addressKey'].' '.$v['attributes']['lastEdited'].'<br/>';
                     }
                 $syncPage++;
             }while($syncPage < $syncData['meta']['totalPageCount']);
