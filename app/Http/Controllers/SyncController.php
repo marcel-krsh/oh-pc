5322f39a015012b21995f9ba880fcbc9b769c2df
marcel-krsh <marcel.krsh@gmail.com>
2018-11-20 22:03:06 -0500

    Key is inside the attributes

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -49,7 +49,7 @@ class SyncController extends Controller
                 foreach($syncData['data'] as $i => $v)
                     {
                         // check if record exists
-                        $updateRecord = SyncMonitoringStatusTypes::select('id')->where('monitoring_status_type_key',$v['monitoringStatusTypeKey'])->first();
+                        $updateRecord = SyncMonitoringStatusTypes::select('id')->where('monitoring_status_type_key',$v['attributes']['monitoringStatusTypeKey'])->first();
 
                         if(isset($updateRecord->id)) {
                             // record exists - update it.
