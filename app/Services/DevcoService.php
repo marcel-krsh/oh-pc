84441793df15f0b87006586f68b6ff60cb9eba92
marcel-krsh <marcel.krsh@gmail.com>
2018-11-20 21:08:15 -0500

    Returns an object - not a string

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -755,7 +755,7 @@ class DevcoService extends PCAPIService
 	 * @param  string|null $device_name
 	 * @return string
 	 */
-	public function listAuditStatuses(int $page=1, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null) : string
+	public function listAuditStatuses(int $page=1, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null) : object
 	{
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
