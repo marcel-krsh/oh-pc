0d19e18835547e68f2896b335d9f0c358c1ce75a
marcel-krsh <marcel.krsh@gmail.com>
2018-11-20 20:56:33 -0500

    no message

--- app/Jobs/SyncAddresses.php
+++ app/Jobs/SyncAddresses.php
@@ -102,6 +102,8 @@ class SyncMonitoringStatusTypeses implements ShouldQueue
                     }
                 $syncPage++;
             }while($syncPage < $syncData['meta']['totalPageCount']);
+        } else {
+            echo "Bad Things";
         }
     }
 }
