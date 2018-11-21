6f20cfe44b8a501bc87bf09c4d73a2790fe8c60a
marcel-krsh <marcel.krsh@gmail.com>
2018-11-20 21:24:29 -0500

    Adding the ing

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -761,7 +761,7 @@ class DevcoService extends PCAPIService
 
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/monitor-status-types?{$params}&{$log_params}");
+		return $this->get("devco/monitoring-status-types?{$params}&{$log_params}");
 	}
 
 	/**
