5077885ffdb0ac0e506a84ecac75a420e15c2f04
marcel-krsh <marcel.krsh@gmail.com>
2018-11-25 02:54:03 -0500

    Project Roles

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -700,7 +700,7 @@ class DevcoService extends PCAPIService
 	 * @param  string|null $device_name
 	 * @return string
 	 */
-	public function listDevelopmentRoles(int $page=1, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null) : string
+	public function listDevelopmentRoles(int $page=1, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null) : object
 	{
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
