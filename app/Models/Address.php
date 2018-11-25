a04d1106ff53e5204b9e84550a4425ad948ce2b4
marcel-krsh <marcel.krsh@gmail.com>
2018-11-25 13:27:20 -0500

    Update Address Model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
+    public $timestamps = true;
+	protected $table = "sync_people";
+    protected $dateFormat = 'Y-m-d\TH:i:s.u';
+
+    
+
     //
+    protected $guarded = ['id'];
 }
