0b6f05668dcfee2662ffc0e63814a8c7cdd9f771
marcel-krsh <marcel.krsh@gmail.com>
2018-11-25 00:15:45 -0500

    -

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -546,7 +546,7 @@ class DevcoService extends PCAPIService
 
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/development_activities?{$params}&{$log_params}");
+		return $this->get("devco/development-activities?{$params}&{$log_params}");
 	}
 
 	/**
