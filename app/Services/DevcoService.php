05e2ea630b264c0e3e74b67a82cd7bcdfd44dadd
marcel-krsh <marcel.krsh@gmail.com>
2018-11-25 01:12:42 -0500

    add activity types

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -550,6 +550,27 @@ class DevcoService extends PCAPIService
 	}
 
 	/**
+	 * List Development Activity Types
+	 * 
+	 * @param  int|integer $page
+	 * @param  string|null $newer_than
+	 * @param  int|null $user
+	 * @param  string|null $user_email
+	 * @param  string|null $user_name
+	 * @param  int|null $device_id
+	 * @param  string|null $device_name
+	 * @return string
+	 */
+	public function listDevelopmentActivityTypes(int $page=1, string $newer_than = null, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null) : object
+	{
+		$params = "page={$page}&newer_than={$newer_than}";
+
+		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
+
+		return $this->get("devco/development-activity-types?{$params}&{$log_params}");
+	}
+
+	/**
 	 * Update Development Activity
 	 * 
 	 * @param  int $development_activity_key
