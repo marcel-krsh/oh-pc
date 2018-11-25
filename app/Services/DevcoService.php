33e88f114e46fc222a0d6018dc104d0c0fe30b06
marcel-krsh <marcel.krsh@gmail.com>
2018-11-25 01:18:17 -0500

    Trying different url

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -567,7 +567,7 @@ class DevcoService extends PCAPIService
 
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/development-activity-types?{$params}&{$log_params}");
+		return $this->get("devco/development_activity_types?{$params}&{$log_params}");
 	}
 
 	/**
