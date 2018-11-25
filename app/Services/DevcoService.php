7ad42ed028fec3e1973aca2fca11ba17a7fca4e2
marcel-krsh <marcel.krsh@gmail.com>
2018-11-25 02:08:49 -0500

    updated url reference

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -567,7 +567,7 @@ class DevcoService extends PCAPIService
 
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/project-activity-types?{$params}&{$log_params}");
+		return $this->get("devco/project_activity_types?{$params}&{$log_params}");
 	}
 
 	/**
