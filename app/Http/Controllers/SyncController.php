5bbe93e355f03f23e8e81eee99ee88af0009e6d4
marcel-krsh <marcel.krsh@gmail.com>
2018-11-25 22:24:29 -0500

    Test updated project activity

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -12,8 +12,8 @@ use App\Models\SystemSetting;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-use App\Models\SyncPeople;
-use App\Models\People;
+use App\Models\SyncProjectActivity;
+use App\Models\ProjectActivity;
 
 
 
@@ -22,7 +22,7 @@ class SyncController extends Controller
     //
     public function sync() {
         //////////////////////////////////////////////////
-        /////// People Sync
+        /////// ProjectActivity Sync
         /////
 
         /// get last modified date inside the database
@@ -33,7 +33,7 @@ class SyncController extends Controller
         /// To do this we use the DB::raw() function and use CONCAT on the column.
         /// We also need to select the column so we can order by it to get the newest first. So we apply an alias to the concated field.
 
-        $lastModifiedDate = SyncPeople::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
+        $lastModifiedDate = SyncProjectActivity::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
         // if the value is null set a default start date to start the sync.
         if(is_null($lastModifiedDate)) {
             $modified = '10/1/1900';
@@ -48,7 +48,7 @@ class SyncController extends Controller
         }
         $apiConnect = new DevcoService();
         if(!is_null($apiConnect)){
-            $syncData = $apiConnect->listPeople(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+            $syncData = $apiConnect->listDevelopmentActivities(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
             $syncData = json_decode($syncData, true);
             $syncPage = 1;
             //dd($syncData);
@@ -57,22 +57,22 @@ class SyncController extends Controller
                 do{
                     if($syncPage > 1){
                         //Get Next Page
-                        $syncData = $apiConnect->listPeople($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+                        $syncData = $apiConnect->listDevelopmentActivities($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
                         $syncData = json_decode($syncData, true);
                         //dd('Page Count is Higher',$syncData);
                     }
                     foreach($syncData['data'] as $i => $v)
                         {
                             // check if record exists
-                            $updateRecord = SyncPeople::select('id','allita_id','last_edited','updated_at')->where('person_key',$v['attributes']['personKey'])->first();
+                            $updateRecord = SyncProjectActivity::select('id','allita_id','last_edited','updated_at')->where('project_activity_key',$v['attributes']['developmentActivityKey'])->first();
                             // convert booleans
-                            settype($v['attributes']['isActive'], 'boolean');
+                            //settype($v['attributes']['isActive'], 'boolean');
                             //dd($updateRecord,$updateRecord->updated_at);
                             if(isset($updateRecord->id)) {
                                 // record exists - get matching table record
 
                                 /// NEW CODE TO UPDATE ALLITA TABLE PART 1
-                                $allitaTableRecord = People::find($updateRecord->allita_id);
+                                $allitaTableRecord = ProjectActivity::find($updateRecord->allita_id);
                                 /// END NEW CODE PART 1
 
                                 // convert dates to seconds and miliseconds to see if the current record is newer.
@@ -93,25 +93,19 @@ class SyncController extends Controller
 
                                         // record is newer than the one currently on file in the allita db.
                                         // update the sync table first
-                                        SyncPeople::where('id',$updateRecord['id'])
+                                        SyncProjectActivity::where('id',$updateRecord['id'])
                                         ->update([
-                                            'is_active'=>$v['attributes']['isActive'],
-                                        'last_name'=>$v['attributes']['lastName'],
-                                    'first_name'=>$v['attributes']['firstName'],
-                                    'default_phone_number_key'=>$v['attributes']['defaultPhoneNumberKey'],
-                                    'default_fax_number_key'=>$v['attributes']['defaultFaxNumberKey'],
-                                    'default_email_address_key'=>$v['attributes']['defaultEmailAddressKey'],
+                                            'project_key'=>$v['attributes']['developmentKey'],
+                                    'project_program_key'=>$v['attributes']['developmentProgramKey'],
+                                    'project_activity_type_key'=>$v['attributes']['projectActivityTypeKey'],
                                         'last_edited'=>$v['attributes']['lastEdited'],
                                         ]);
-                                        $UpdateAllitaValues = SyncPeople::find($updateRecord['id']);
+                                        $UpdateAllitaValues = SyncProjectActivity::find($updateRecord['id']);
                                         // update the allita db - we use the updated at of the sync table as the last edited value for the actual Allita Table.
                                         $allitaTableRecord->update([
-                                            'is_active'=>$v['attributes']['isActive'],
-                                            'last_name'=>$v['attributes']['lastName'],
-                                    'first_name'=>$v['attributes']['firstName'],
-                                    'default_phone_number_key'=>$v['attributes']['defaultPhoneNumberKey'],
-                                    'default_fax_number_key'=>$v['attributes']['defaultFaxNumberKey'],
-                                    'default_email_address_key'=>$v['attributes']['defaultEmailAddressKey'],
+                                            'project_key'=>$v['attributes']['developmentKey'],
+                                    'project_program_key'=>$v['attributes']['developmentProgramKey'],
+                                    'project_activity_type_key'=>$v['attributes']['projectActivityTypeKey'],
                                             'last_edited'=>$UpdateAllitaValues->updated_at,
                                         ]);
                                         //dd('inside.');
@@ -122,25 +116,20 @@ class SyncController extends Controller
                                         // date ends up in the allita table record
                                         // (if we create the sync record first the updated at date would become out of sync with the allita table.)
 
-                                        $allitaTableRecord = People::create([
-                                            'person_key'=>$v['attributes']['personKey'],
-                                            'is_active'=>$v['attributes']['isActive'],
-                                            'last_name'=>$v['attributes']['lastName'],
-                                    'first_name'=>$v['attributes']['firstName'],
-                                    'default_phone_number_key'=>$v['attributes']['defaultPhoneNumberKey'],
-                                    'default_fax_number_key'=>$v['attributes']['defaultFaxNumberKey'],
+                                        $allitaTableRecord = ProjectActivity::create([
+                                            'project_activity_key'=>$v['attributes']['developmentActivityKey'],
+                                            'project_key'=>$v['attributes']['developmentKey'],
+                                    'project_program_key'=>$v['attributes']['developmentProgramKey'],
+                                    'project_activity_type_key'=>$v['attributes']['projectActivityTypeKey'],
                                     'default_email_address_key'=>$v['attributes']['defaultEmailAddressKey'],
                                         ]);
                                         // Create the sync table entry with the allita id
-                                        $syncTableRecord = SyncPeople::where('id',$updateRecord['id'])
+                                        $syncTableRecord = SyncProjectActivity::where('id',$updateRecord['id'])
                                         ->update([
-                                            'person_key'=>$v['attributes']['personKey'],
-                                            'is_active'=>$v['attributes']['isActive'],
-                                            'last_name'=>$v['attributes']['lastName'],
-                                    'first_name'=>$v['attributes']['firstName'],
-                                    'default_phone_number_key'=>$v['attributes']['defaultPhoneNumberKey'],
-                                    'default_fax_number_key'=>$v['attributes']['defaultFaxNumberKey'],
-                                    'default_email_address_key'=>$v['attributes']['defaultEmailAddressKey'],
+                                            'project_activity_key'=>$v['attributes']['developmentActivityKey'],
+                                            'project_key'=>$v['attributes']['developmentKey'],
+                                    'project_program_key'=>$v['attributes']['developmentProgramKey'],
+                                    'project_activity_type_key'=>$v['attributes']['projectActivityTypeKey'],
                                             'last_edited'=>$v['attributes']['lastEdited'],
                                         ]);                                     
                                         // Update the Allita Table Record with the Sync Table's updated at date
@@ -155,25 +144,19 @@ class SyncController extends Controller
                                 // Create the Allita Entry First
                                 // We do this so the updated_at value of the Sync Table does not become newer
                                 // when we add in the allita_id
-                                $allitaTableRecord = People::create([
-                                    'person_key'=>$v['attributes']['personKey'],
-                                    'is_active'=>$v['attributes']['isActive'],
-                                        'last_name'=>$v['attributes']['lastName'],
-                                    'first_name'=>$v['attributes']['firstName'],
-                                    'default_phone_number_key'=>$v['attributes']['defaultPhoneNumberKey'],
-                                    'default_fax_number_key'=>$v['attributes']['defaultFaxNumberKey'],
+                                $allitaTableRecord = ProjectActivity::create([
+                                    'project_activity_key'=>$v['attributes']['developmentActivityKey'],
+                                    'project_key'=>$v['attributes']['developmentKey'],
+                                    'project_program_key'=>$v['attributes']['developmentProgramKey'],
+                                    'project_activity_type_key'=>$v['attributes']['projectActivityTypeKey'],
                                     'default_email_address_key'=>$v['attributes']['defaultEmailAddressKey'],
                                 ]);
                                 // Create the sync table entry with the allita id
-                                $syncTableRecord = SyncPeople::create([
-                                    'person_key'=>$v['attributes']['personKey'],
-                                    'is_active'=>$v['attributes']['isActive'],
-                                        'allita_id'=>$allitaTableRecord->id,
-                                        'last_name'=>$v['attributes']['lastName'],
-                                    'first_name'=>$v['attributes']['firstName'],
-                                    'default_phone_number_key'=>$v['attributes']['defaultPhoneNumberKey'],
-                                    'default_fax_number_key'=>$v['attributes']['defaultFaxNumberKey'],
-                                    'default_email_address_key'=>$v['attributes']['defaultEmailAddressKey'],
+                                $syncTableRecord = SyncProjectActivity::create([
+                                    'project_activity_key'=>$v['attributes']['developmentActivityKey'],
+                                    'project_key'=>$v['attributes']['developmentKey'],
+                                    'project_program_key'=>$v['attributes']['developmentProgramKey'],
+                                    'project_activity_type_key'=>$v['attributes']['projectActivityTypeKey'],
                                         'last_edited'=>$v['attributes']['lastEdited'],
                                 ]);
                                 // Update the Allita Table Record with the Sync Table's updated at date
