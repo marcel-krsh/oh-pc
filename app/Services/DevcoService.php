3b1cd47cb74a82b608f45b39bacc92b66b397f5d
marcel-krsh <marcel.krsh@gmail.com>
2018-11-26 00:52:39 -0500

    dd crap

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -805,7 +805,8 @@ class DevcoService extends PCAPIService
 
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/organizations?{$params}&{$log_params}");
+		$crap = $this->get("devco/organizations?{$params}&{$log_params}");
+		dd($crap);
 	}
 
 	/**
