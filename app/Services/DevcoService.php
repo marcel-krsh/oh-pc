9015e513c1ad801571817ef0793ad4cd46c621ea
marcel-krsh <marcel.krsh@gmail.com>
2018-11-29 19:57:51 -0500

    test

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -500,9 +500,11 @@ class DevcoService extends PCAPIService
 	 */
 	public function listDevelopmentAmenities(int $page = 1, string $newer_than = null, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, int $development_key=null) : object
 	{
+		$params = "page={$page}&newer_than={$newer_than}";
+
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/developments/{$development_key}/amenities?page={$page}&{$log_params}");
+		return $this->get("devco/developments/{$development_key}/amenities?{$params}&{$log_params}");
 	}
 
 	/**
