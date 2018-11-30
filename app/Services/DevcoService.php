875b2db7a90812e97e6c63331dbc246abceb83f9
marcel-krsh <marcel.krsh@gmail.com>
2018-11-29 19:28:19 -0500

    fix missing reference

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -498,7 +498,7 @@ class DevcoService extends PCAPIService
 	 * @param  string|null $device_name
 	 * @return string
 	 */
-	public function listDevelopmentAmenities(int $page = 1, string $newer_than = null, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null) : object
+	public function listDevelopmentAmenities(int $page = 1, string $newer_than = null, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null, int $development_key=null) : object
 	{
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
