62d8e4e65b19d1c446cdeaf70ea670e73cf88bb1
marcel-krsh <marcel.krsh@gmail.com>
2018-12-04 11:51:52 -0500

    Putting in the initial multiple building election types

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -12,8 +12,8 @@ use App\Models\SystemSetting;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-use App\Models\SyncProgramDateType;
-use App\Models\ProgramDateType;
+use App\Models\SyncMultipleBuildingElectionType;
+use App\Models\MultipleBuildingElectionType;
 
 
 
@@ -22,7 +22,7 @@ class SyncController extends Controller
     //
     public function sync() {
         //////////////////////////////////////////////////
-        /////// ProgramDateType Sync
+        /////// MultipleBuildingElectionType Sync
         /////
 
         /// get last modified date inside the database
@@ -33,7 +33,7 @@ class SyncController extends Controller
         /// To do this we use the DB::raw() function and use CONCAT on the column.
         /// We also need to select the column so we can order by it to get the newest first. So we apply an alias to the concated field.
 
-        $lastModifiedDate = SyncProgramDateType::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
+        $lastModifiedDate = SyncMultipleBuildingElectionType::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
         // if the value is null set a default start date to start the sync.
         if(is_null($lastModifiedDate)) {
             $modified = '10/1/1900';
@@ -48,23 +48,23 @@ class SyncController extends Controller
         }
         $apiConnect = new DevcoService();
         if(!is_null($apiConnect)){
-            $syncData = $apiConnect->listProgramDateTypes(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+            $syncData = $apiConnect->listMultipleBuildingElectionTypes(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
             $syncData = json_decode($syncData, true);
             $syncPage = 1;
-            //dd($syncData);
+            dd($syncData);
             //dd($lastModifiedDate->last_edited_convert,$currentModifiedDateTimeStamp,$modified,$syncData);
             if($syncData['meta']['totalPageCount'] > 0){
                 do{
                     if($syncPage > 1){
                         //Get Next Page
-                        $syncData = $apiConnect->listProgramDateTypes($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+                        $syncData = $apiConnect->listMultipleBuildingElectionTypes($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
                         $syncData = json_decode($syncData, true);
                         dd('Page Count is Higher',$syncData);
                     }
                     foreach($syncData['data'] as $i => $v)
                         {
                             // check if record exists
-                            $updateRecord = SyncProgramDateType::select('id','allita_id','last_edited','updated_at')->where('program_date_type_key',$v['attributes']['programDateTypeKey'])->first();
+                            $updateRecord = SyncMultipleBuildingElectionType::select('id','allita_id','last_edited','updated_at')->where('program_date_type_key',$v['attributes']['programDateTypeKey'])->first();
                             // convert booleans
                             //settype($v['attributes']['isActive'], 'boolean');
                             //dd($updateRecord,$updateRecord->updated_at);
@@ -72,7 +72,7 @@ class SyncController extends Controller
                                 // record exists - get matching table record
 
                                 /// NEW CODE TO UPDATE ALLITA TABLE PART 1
-                                $allitaTableRecord = ProgramDateType::find($updateRecord->allita_id);
+                                $allitaTableRecord = MultipleBuildingElectionType::find($updateRecord->allita_id);
                                 /// END NEW CODE PART 1
 
                                 // convert dates to seconds and miliseconds to see if the current record is newer.
@@ -93,14 +93,14 @@ class SyncController extends Controller
 
                                         // record is newer than the one currently on file in the allita db.
                                         // update the sync table first
-                                        SyncProgramDateType::where('id',$updateRecord['id'])
+                                        SyncMultipleBuildingElectionType::where('id',$updateRecord['id'])
                                         ->update([
                                             'date_name'=>$v['attributes']['dateName'],
                                             'date_description'=>$v['attributes']['dateDescription'],
                                             
                                             'last_edited'=>$v['attributes']['lastEdited'],
                                         ]);
-                                        $UpdateAllitaValues = SyncProgramDateType::find($updateRecord['id']);
+                                        $UpdateAllitaValues = SyncMultipleBuildingElectionType::find($updateRecord['id']);
                                         // update the allita db - we use the updated at of the sync table as the last edited value for the actual Allita Table.
                                         $allitaTableRecord->update([
                                             'date_name'=>$v['attributes']['dateName'],
@@ -116,7 +116,7 @@ class SyncController extends Controller
                                         // date ends up in the allita table record
                                         // (if we create the sync record first the updated at date would become out of sync with the allita table.)
 
-                                        $allitaTableRecord = ProgramDateType::create([
+                                        $allitaTableRecord = MultipleBuildingElectionType::create([
                                             'date_name'=>$v['attributes']['dateName'],
                                             
                                             'date_description'=>$v['attributes']['dateDescription'],
@@ -124,7 +124,7 @@ class SyncController extends Controller
                                             'program_date_type_key'=>$v['attributes']['programDateTypeKey'],
                                         ]);
                                         // Create the sync table entry with the allita id
-                                        $syncTableRecord = SyncProgramDateType::where('id',$updateRecord['id'])
+                                        $syncTableRecord = SyncMultipleBuildingElectionType::where('id',$updateRecord['id'])
                                         ->update([
                                             'date_name'=>$v['attributes']['dateName'],
                                             
@@ -146,7 +146,7 @@ class SyncController extends Controller
                                 // Create the Allita Entry First
                                 // We do this so the updated_at value of the Sync Table does not become newer
                                 // when we add in the allita_id
-                                $allitaTableRecord = ProgramDateType::create([
+                                $allitaTableRecord = MultipleBuildingElectionType::create([
                                     'date_name'=>$v['attributes']['dateName'],
 
                                             'date_description'=>$v['attributes']['dateDescription'],
@@ -154,7 +154,7 @@ class SyncController extends Controller
                                     'program_date_type_key'=>$v['attributes']['programDateTypeKey'],
                                 ]);
                                 // Create the sync table entry with the allita id
-                                $syncTableRecord = SyncProgramDateType::create([
+                                $syncTableRecord = SyncMultipleBuildingElectionType::create([
                                             'date_name'=>$v['attributes']['dateName'],
                                             
                                             'date_description'=>$v['attributes']['dateDescription'],
