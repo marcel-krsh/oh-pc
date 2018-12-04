5951e05b246b53b4829f1373da855ed80d2959d0
marcel-krsh <marcel.krsh@gmail.com>
2018-12-04 16:14:24 -0500

    set to percentages

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -810,7 +810,7 @@ class DevcoService extends PCAPIService
 
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/percentage/?{$log_params}&{$params}");
+		return $this->get("devco/percentages/?{$log_params}&{$params}");
 	}
 
 	/**
