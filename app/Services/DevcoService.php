5ae63e372f1c25c28ebb8cfe57805ce13aeb7a21
marcel-krsh <marcel.krsh@gmail.com>
2018-12-04 12:10:17 -0500

    Add percentages endpoint

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -794,6 +794,26 @@ class DevcoService extends PCAPIService
 	}
 
 	/**
+	 * List Percentages
+	 * 
+	 * @param  int|integer $page
+	 * @param  int|null $user
+	 * @param  string|null $user_email
+	 * @param  string|null $user_name
+	 * @param  int|null $device_id
+	 * @param  string|null $device_name
+	 * @return object
+	 */
+	public function listPercentages(int $page = 1, string $newer_than = null, int $user=null, string $user_email=null, string $user_name=null, int $device_id=null, string $device_name=null) : object
+	{
+		$params = "page={$page}&newer_than={$newer_than}";
+
+		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
+
+		return $this->get("devco/percentages/?{$log_params}&{$params}");
+	}
+
+	/**
 	 * Update Development Program
 	 * 
 	 * @param  int $development_program_key
