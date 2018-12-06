6d79b7f52dffa19f8906c86808f4f792955800e8
marcel-krsh <marcel.krsh@gmail.com>
2018-12-06 16:39:28 -0500

    fix model names

--- app/Models/HouseHoldEvent.php
+++ app/Models/HouseHoldEvent.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class HouseHoldEvent extends Model
+class HouseholdEvent extends Model
 {
     public $timestamps = true;
 	//protected $dateFormat = 'Y-m-d\TH:i:s.u';
