b0d31961591073d1114166d675ce3d1f7549ff2e
marcel-krsh <marcel.krsh@gmail.com>
2018-12-09 15:28:23 -0500

    add sync job to the kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ use App\Jobs\SyncUnitsJob;
 use App\Jobs\SyncUnitBedroomsJob;
 use App\Jobs\SyncHouseholdEventsJob;
 use App\Jobs\SyncOwnerCertificationYearsJob;
+use App\Jobs\SyncHouseholdsJob;
 
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -245,6 +246,15 @@ class Kernel extends ConsoleKernel
         $test = DB::table('jobs')->where('payload','like','%SyncOwnerCertificationYearsJob%')->first();
         if(is_null($test)) {
             $schedule->job(new SyncOwnerCertificationYearsJob)->everyMinute();
+            
+        } else {
+            //Log::info('Sync Job Already Started.');
+        }
+
+        //SyncHouseholdsJob
+        $test = DB::table('jobs')->where('payload','like','%SyncHouseholdsJob%')->first();
+        if(is_null($test)) {
+            $schedule->job(new SyncHouseholdsJob)->everyMinute();
             
         } else {
             //Log::info('Sync Job Already Started.');
