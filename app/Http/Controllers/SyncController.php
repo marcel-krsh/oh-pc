86dd00e16a3a55c0f300e027a59f8cbedb9a114f
marcel-krsh <marcel.krsh@gmail.com>
2018-12-09 15:02:11 -0500

    start of household sync

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -12,8 +12,8 @@ use App\Models\SystemSetting;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-use App\Models\SyncOwnerCertificationYear;
-use App\Models\OwnerCertificationYear;
+use App\Models\SyncHousehold;
+use App\Models\Household;
 
 
 
@@ -22,7 +22,7 @@ class SyncController extends Controller
     //
     public function sync() {
         //////////////////////////////////////////////////
-        /////// OwnerCertificationYear Sync
+        /////// Household Sync
         /////
 
         /// get last modified date inside the database
@@ -33,7 +33,7 @@ class SyncController extends Controller
         /// To do this we use the DB::raw() function and use CONCAT on the column.
         /// We also need to select the column so we can order by it to get the newest first. So we apply an alias to the concated field.
 
-        $lastModifiedDate = SyncOwnerCertificationYear::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
+        $lastModifiedDate = SyncHousehold::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
         // if the value is null set a default start date to start the sync.
         if(is_null($lastModifiedDate)) {
             $modified = '10/1/1900';
@@ -48,16 +48,16 @@ class SyncController extends Controller
         }
         $apiConnect = new DevcoService();
         if(!is_null($apiConnect)){
-            $syncData = $apiConnect->listOwnerCertificationYears(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+            $syncData = $apiConnect->listHouseholds(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
             $syncData = json_decode($syncData, true);
             $syncPage = 1;
-            //dd($syncData);
+            dd($syncData);
             //dd($lastModifiedDate->last_edited_convert,$currentModifiedDateTimeStamp,$modified,$syncData);
             if($syncData['meta']['totalPageCount'] > 0){
                 do{
                     if($syncPage > 1){
                         //Get Next Page
-                        $syncData = $apiConnect->listOwnerCertificationYears($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+                        $syncData = $apiConnect->listHouseholds($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
                         $syncData = json_decode($syncData, true);
                         //dd('Page Count is Higher',$syncData,$syncData['meta']['totalPageCount'],$syncPage);
                     }
@@ -65,16 +65,16 @@ class SyncController extends Controller
                     foreach($syncData['data'] as $i => $v)
                         {
                             // check if record exists
-                            $updateRecord = SyncOwnerCertificationYear::select('id','allita_id','last_edited','updated_at')->where('owner_certification_year_key',$v['attributes']['ownerCertificationYearKey'])->first();
+                            $updateRecord = SyncHousehold::select('id','allita_id','last_edited','updated_at')->where('owner_certification_year_key',$v['attributes']['ownerCertificationYearKey'])->first();
                             // convert booleans
                             // settype($v['attributes']['isActive'], 'boolean');
-                            // settype($v['attributes']['isOwnerCertificationYearHandicapAccessible'], 'boolean');
+                            // settype($v['attributes']['isHouseholdHandicapAccessible'], 'boolean');
                             //dd($updateRecord,$updateRecord->updated_at);
                             if(isset($updateRecord->id)) {
                                 // record exists - get matching table record
 
                                 /// NEW CODE TO UPDATE ALLITA TABLE PART 1
-                                $allitaTableRecord = OwnerCertificationYear::find($updateRecord->allita_id);
+                                $allitaTableRecord = Household::find($updateRecord->allita_id);
                                 /// END NEW CODE PART 1
 
                                 // convert dates to seconds and miliseconds to see if the current record is newer.
@@ -95,7 +95,7 @@ class SyncController extends Controller
 
                                         // record is newer than the one currently on file in the allita db.
                                         // update the sync table first
-                                        SyncOwnerCertificationYear::where('id',$updateRecord['id'])
+                                        SyncHousehold::where('id',$updateRecord['id'])
                                         ->update([
                                             
                                             
@@ -106,7 +106,7 @@ class SyncController extends Controller
                                             
                                             'last_edited'=>$v['attributes']['lastEdited'],
                                         ]);
-                                        $UpdateAllitaValues = SyncOwnerCertificationYear::find($updateRecord['id']);
+                                        $UpdateAllitaValues = SyncHousehold::find($updateRecord['id']);
                                         // update the allita db - we use the updated at of the sync table as the last edited value for the actual Allita Table.
                                         $allitaTableRecord->update([
                                             
@@ -126,7 +126,7 @@ class SyncController extends Controller
                                         // date ends up in the allita table record
                                         // (if we create the sync record first the updated at date would become out of sync with the allita table.)
 
-                                        $allitaTableRecord = OwnerCertificationYear::create([
+                                        $allitaTableRecord = Household::create([
                                             
                                             
                                             
@@ -138,7 +138,7 @@ class SyncController extends Controller
                                             'owner_certification_year_key'=>$v['attributes']['ownerCertificationYearKey'],
                                         ]);
                                         // Create the sync table entry with the allita id
-                                        $syncTableRecord = SyncOwnerCertificationYear::where('id',$updateRecord['id'])
+                                        $syncTableRecord = SyncHousehold::where('id',$updateRecord['id'])
                                         ->update([
                                             
                                             
@@ -164,7 +164,7 @@ class SyncController extends Controller
                                 // Create the Allita Entry First
                                 // We do this so the updated_at value of the Sync Table does not become newer
                                 // when we add in the allita_id
-                                $allitaTableRecord = OwnerCertificationYear::create([
+                                $allitaTableRecord = Household::create([
                                     
 
                                             
@@ -176,7 +176,7 @@ class SyncController extends Controller
                                     'owner_certification_year_key'=>$v['attributes']['ownerCertificationYearKey'],
                                 ]);
                                 // Create the sync table entry with the allita id
-                                $syncTableRecord = SyncOwnerCertificationYear::create([
+                                $syncTableRecord = SyncHousehold::create([
                                             
                                             
                                             
