f23ce598891350189559ed2b5761335a02076ec3
marcel-krsh <marcel.krsh@gmail.com>
2018-12-10 15:54:24 -0500

    Start sync of allowances

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -12,8 +12,8 @@ use App\Models\SystemSetting;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-use App\Models\SyncRentalAssistanceType;
-use App\Models\RentalAssistanceType;
+use App\Models\SyncUtilityAllowance;
+use App\Models\UtilityAllowance;
 
 
 
@@ -22,7 +22,7 @@ class SyncController extends Controller
     //
     public function sync() {
         //////////////////////////////////////////////////
-        /////// RentalAssistanceType Sync
+        /////// UtilityAllowance Sync
         /////
 
         /// get last modified date inside the database
@@ -33,7 +33,7 @@ class SyncController extends Controller
         /// To do this we use the DB::raw() function and use CONCAT on the column.
         /// We also need to select the column so we can order by it to get the newest first. So we apply an alias to the concated field.
 
-        $lastModifiedDate = SyncRentalAssistanceType::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
+        $lastModifiedDate = SyncUtilityAllowance::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
         // if the value is null set a default start date to start the sync.
         if(is_null($lastModifiedDate)) {
             $modified = '10/1/1900';
@@ -48,16 +48,16 @@ class SyncController extends Controller
         }
         $apiConnect = new DevcoService();
         if(!is_null($apiConnect)){
-            $syncData = $apiConnect->listRentalAssistanceTypes(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+            $syncData = $apiConnect->listUtilityAllowances(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
             $syncData = json_decode($syncData, true);
             $syncPage = 1;
-            //dd($syncData);
+            dd($syncData);
             //dd($lastModifiedDate->last_edited_convert,$currentModifiedDateTimeStamp,$modified,$syncData);
             if($syncData['meta']['totalPageCount'] > 0){
                 do{
                     if($syncPage > 1){
                         //Get Next Page
-                        $syncData = $apiConnect->listRentalAssistanceTypes($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+                        $syncData = $apiConnect->listUtilityAllowances($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
                         $syncData = json_decode($syncData, true);
                         //dd('Page Count is Higher',$syncData,$syncData['meta']['totalPageCount'],$syncPage);
                     }
@@ -65,16 +65,16 @@ class SyncController extends Controller
                     foreach($syncData['data'] as $i => $v)
                         {
                             // check if record exists
-                            $updateRecord = SyncRentalAssistanceType::select('id','allita_id','last_edited','updated_at')->where('rental_assistance_type_key',$v['attributes']['rentalAssistanceTypeKey'])->first();
+                            $updateRecord = SyncUtilityAllowance::select('id','allita_id','last_edited','updated_at')->where('rental_assistance_type_key',$v['attributes']['rentalAssistanceTypeKey'])->first();
                             // convert booleans
                             // settype($v['attributes']['isActive'], 'boolean');
-                            // settype($v['attributes']['isRentalAssistanceTypeHandicapAccessible'], 'boolean');
+                            // settype($v['attributes']['isUtilityAllowanceHandicapAccessible'], 'boolean');
                             //dd($updateRecord,$updateRecord->updated_at);
                             if(isset($updateRecord->id)) {
                                 // record exists - get matching table record
 
                                 /// NEW CODE TO UPDATE ALLITA TABLE PART 1
-                                $allitaTableRecord = RentalAssistanceType::find($updateRecord->allita_id);
+                                $allitaTableRecord = UtilityAllowance::find($updateRecord->allita_id);
                                 /// END NEW CODE PART 1
 
                                 // convert dates to seconds and miliseconds to see if the current record is newer.
@@ -95,7 +95,7 @@ class SyncController extends Controller
 
                                         // record is newer than the one currently on file in the allita db.
                                         // update the sync table first
-                                        SyncRentalAssistanceType::where('id',$updateRecord['id'])
+                                        SyncUtilityAllowance::where('id',$updateRecord['id'])
                                         ->update([
                                             
                                             
@@ -107,7 +107,7 @@ class SyncController extends Controller
                                             
                                             'last_edited'=>$v['attributes']['lastEdited'],
                                         ]);
-                                        $UpdateAllitaValues = SyncRentalAssistanceType::find($updateRecord['id']);
+                                        $UpdateAllitaValues = SyncUtilityAllowance::find($updateRecord['id']);
                                         // update the allita db - we use the updated at of the sync table as the last edited value for the actual Allita Table.
                                         $allitaTableRecord->update([
                                             
@@ -128,7 +128,7 @@ class SyncController extends Controller
                                         // date ends up in the allita table record
                                         // (if we create the sync record first the updated at date would become out of sync with the allita table.)
 
-                                        $allitaTableRecord = RentalAssistanceType::create([
+                                        $allitaTableRecord = UtilityAllowance::create([
                                             
                                             
                                             
@@ -141,7 +141,7 @@ class SyncController extends Controller
                                             'rental_assistance_type_key'=>$v['attributes']['rentalAssistanceTypeKey'],
                                         ]);
                                         // Create the sync table entry with the allita id
-                                        $syncTableRecord = SyncRentalAssistanceType::where('id',$updateRecord['id'])
+                                        $syncTableRecord = SyncUtilityAllowance::where('id',$updateRecord['id'])
                                         ->update([
                                             
                                             
@@ -168,7 +168,7 @@ class SyncController extends Controller
                                 // Create the Allita Entry First
                                 // We do this so the updated_at value of the Sync Table does not become newer
                                 // when we add in the allita_id
-                                $allitaTableRecord = RentalAssistanceType::create([
+                                $allitaTableRecord = UtilityAllowance::create([
                                     
 
                                             
@@ -181,7 +181,7 @@ class SyncController extends Controller
                                     'rental_assistance_type_key'=>$v['attributes']['rentalAssistanceTypeKey'],
                                 ]);
                                 // Create the sync table entry with the allita id
-                                $syncTableRecord = SyncRentalAssistanceType::create([
+                                $syncTableRecord = SyncUtilityAllowance::create([
                                             
                                             
                                             
