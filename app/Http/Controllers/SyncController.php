13be5b8a052f9e0605a47f289a60be309fb11802
marcel-krsh <marcel.krsh@gmail.com>
2018-12-11 12:46:16 -0500

    Start sync of Monitoring/Audit

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -12,8 +12,8 @@ use App\Models\SystemSetting;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-use App\Models\SyncUtilityAllowance;
-use App\Models\UtilityAllowance;
+use App\Models\SyncMonitoring;
+use App\Models\Audit;
 
 
 
@@ -22,7 +22,7 @@ class SyncController extends Controller
     //
     public function sync() {
         //////////////////////////////////////////////////
-        /////// UtilityAllowance Sync
+        /////// Audit Sync
         /////
 
         /// get last modified date inside the database
@@ -33,7 +33,7 @@ class SyncController extends Controller
         /// To do this we use the DB::raw() function and use CONCAT on the column.
         /// We also need to select the column so we can order by it to get the newest first. So we apply an alias to the concated field.
 
-        $lastModifiedDate = SyncUtilityAllowance::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
+        $lastModifiedDate = SyncMonitoring::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
         // if the value is null set a default start date to start the sync.
         if(is_null($lastModifiedDate)) {
             $modified = '10/1/1900';
@@ -48,16 +48,16 @@ class SyncController extends Controller
         }
         $apiConnect = new DevcoService();
         if(!is_null($apiConnect)){
-            $syncData = $apiConnect->listUtilityAllowances(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+            $syncData = $apiConnect->listAudits(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
             $syncData = json_decode($syncData, true);
             $syncPage = 1;
-            //dd($syncData);
+            dd($syncData);
             //dd($lastModifiedDate->last_edited_convert,$currentModifiedDateTimeStamp,$modified,$syncData);
             if($syncData['meta']['totalPageCount'] > 0){
                 do{
                     if($syncPage > 1){
                         //Get Next Page
-                        $syncData = $apiConnect->listUtilityAllowances($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+                        $syncData = $apiConnect->listAudits($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
                         $syncData = json_decode($syncData, true);
                         //dd('Page Count is Higher',$syncData,$syncData['meta']['totalPageCount'],$syncPage);
                     }
@@ -65,10 +65,10 @@ class SyncController extends Controller
                     foreach($syncData['data'] as $i => $v)
                         {
                             // check if record exists
-                            $updateRecord = SyncUtilityAllowance::select('id','allita_id','last_edited','updated_at')->where('utility_allowance_key',$v['attributes']['utilityAllowanceKey'])->first();
+                            $updateRecord = SyncMonitoring::select('id','allita_id','last_edited','updated_at')->where('utility_allowance_key',$v['attributes']['utilityAllowanceKey'])->first();
                             // convert booleans
                             // settype($v['attributes']['isActive'], 'boolean');
-                            // settype($v['attributes']['isUtilityAllowanceHandicapAccessible'], 'boolean');
+                            // settype($v['attributes']['isAuditHandicapAccessible'], 'boolean');
 
                             // Set dates older than 1950 to be NULL:
                             if($v['attributes']['effectiveDate'] < 1951){
@@ -82,7 +82,7 @@ class SyncController extends Controller
                                 // record exists - get matching table record
 
                                 /// NEW CODE TO UPDATE ALLITA TABLE PART 1
-                                $allitaTableRecord = UtilityAllowance::find($updateRecord->allita_id);
+                                $allitaTableRecord = Audit::find($updateRecord->allita_id);
                                 /// END NEW CODE PART 1
 
                                 // convert dates to seconds and miliseconds to see if the current record is newer.
@@ -103,7 +103,7 @@ class SyncController extends Controller
 
                                         // record is newer than the one currently on file in the allita db.
                                         // update the sync table first
-                                        SyncUtilityAllowance::where('id',$updateRecord['id'])
+                                        SyncMonitoring::where('id',$updateRecord['id'])
                                         ->update([
                                             
                                             
@@ -118,7 +118,7 @@ class SyncController extends Controller
                                             
                                             'last_edited'=>$v['attributes']['lastEdited'],
                                         ]);
-                                        $UpdateAllitaValues = SyncUtilityAllowance::find($updateRecord['id']);
+                                        $UpdateAllitaValues = SyncMonitoring::find($updateRecord['id']);
                                         // update the allita db - we use the updated at of the sync table as the last edited value for the actual Allita Table.
                                         $allitaTableRecord->update([
                                             
@@ -142,7 +142,7 @@ class SyncController extends Controller
                                         // date ends up in the allita table record
                                         // (if we create the sync record first the updated at date would become out of sync with the allita table.)
 
-                                        $allitaTableRecord = UtilityAllowance::create([
+                                        $allitaTableRecord = Audit::create([
                                             
                                             
                                             
@@ -158,7 +158,7 @@ class SyncController extends Controller
                                             'utility_allowance_key'=>$v['attributes']['utilityAllowanceKey'],
                                         ]);
                                         // Create the sync table entry with the allita id
-                                        $syncTableRecord = SyncUtilityAllowance::where('id',$updateRecord['id'])
+                                        $syncTableRecord = SyncMonitoring::where('id',$updateRecord['id'])
                                         ->update([
                                             
                                             
@@ -188,7 +188,7 @@ class SyncController extends Controller
                                 // Create the Allita Entry First
                                 // We do this so the updated_at value of the Sync Table does not become newer
                                 // when we add in the allita_id
-                                $allitaTableRecord = UtilityAllowance::create([
+                                $allitaTableRecord = Audit::create([
                                     
 
                                             
@@ -204,7 +204,7 @@ class SyncController extends Controller
                                     'utility_allowance_key'=>$v['attributes']['utilityAllowanceKey'],
                                 ]);
                                 // Create the sync table entry with the allita id
-                                $syncTableRecord = SyncUtilityAllowance::create([
+                                $syncTableRecord = SyncMonitoring::create([
                                             
                                             
                                             
