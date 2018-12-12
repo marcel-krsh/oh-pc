13872cc836dae397f46735c847fb3eae45f7ea49
marcel-krsh <marcel.krsh@gmail.com>
2018-12-12 14:40:45 -0500

    start building amenity setup

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -1669,7 +1669,7 @@ class DevcoService extends PCAPIService
 
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/unit-amenities?{$params}&{$log_params}");
+		return $this->get("devco/unit_amenities?{$params}&{$log_params}");
 	}
 
 	/**
