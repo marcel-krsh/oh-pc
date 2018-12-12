5af32f188637bb9de8b394e512478dde440b5c57
marcel-krsh <marcel.krsh@gmail.com>
2018-12-12 13:59:06 -0500

    Initial unit amenities sync

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -12,8 +12,8 @@ use App\Models\SystemSetting;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-use App\Models\SyncMonitoringMonitor;
-use App\Models\AuditAuditor;
+use App\Models\SyncUnitAmenity;
+use App\Models\UnitAmenity;
 
 
 class SyncController extends Controller
@@ -21,7 +21,7 @@ class SyncController extends Controller
     //
     public function sync() {
         //////////////////////////////////////////////////
-        /////// AuditAuditor Sync
+        /////// UnitAmenity Sync
         /////
 
         /// get last modified date inside the database
@@ -32,7 +32,7 @@ class SyncController extends Controller
         /// To do this we use the DB::raw() function and use CONCAT on the column.
         /// We also need to select the column so we can order by it to get the newest first. So we apply an alias to the concated field.
 
-        $lastModifiedDate = SyncMonitoringMonitor::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
+        $lastModifiedDate = SyncUnitAmenity::select(DB::raw("CONCAT(last_edited) as 'last_edited_convert'"),'last_edited','id')->orderBy('last_edited','desc')->first();
         // if the value is null set a default start date to start the sync.
         if(is_null($lastModifiedDate)) {
             $modified = '10/1/1900';
@@ -47,7 +47,7 @@ class SyncController extends Controller
         }
         $apiConnect = new DevcoService();
         if(!is_null($apiConnect)){
-            $syncData = $apiConnect->listMonitoringMonitors(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+            $syncData = $apiConnect->listUnitAmenitiess(1, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
             $syncData = json_decode($syncData, true);
             $syncPage = 1;
             //dd($syncData);
@@ -56,7 +56,7 @@ class SyncController extends Controller
                 do{
                     if($syncPage > 1){
                         //Get Next Page
-                        $syncData = $apiConnect->listMonitoringMonitors($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
+                        $syncData = $apiConnect->listUnitAmenitiess($syncPage, $modified, 1,'admin@allita.org', 'System Sync Job', 1, 'Server');
                         $syncData = json_decode($syncData, true);
                         //dd('Page Count is Higher',$syncData,$syncData['meta']['totalPageCount'],$syncPage);
                     }
@@ -64,10 +64,10 @@ class SyncController extends Controller
                     foreach($syncData['data'] as $i => $v)
                         {
                             // check if record exists
-                            $updateRecord = SyncMonitoringMonitor::select('id','allita_id','last_edited','updated_at')->where('monitoring_monitor_key',$v['attributes']['monitoringMonitorKey'])->first();
+                            $updateRecord = SyncUnitAmenity::select('id','allita_id','last_edited','updated_at')->where('unit_amenity_key',$v['attributes']['unitAmenityKey'])->first();
                             // convert booleans
                             // settype($v['attributes']['isActive'], 'boolean');
-                            // settype($v['attributes']['isAuditAuditorHandicapAccessible'], 'boolean');
+                            // settype($v['attributes']['isUnitAmenityHandicapAccessible'], 'boolean');
 
                             // Set dates older than 1950 to be NULL:
                             // if($v['attributes']['comment'] < 1951){
@@ -87,7 +87,7 @@ class SyncController extends Controller
                                 // record exists - get matching table record
 
                                 /// NEW CODE TO UPDATE ALLITA TABLE PART 1
-                                $allitaTableRecord = AuditAuditor::find($updateRecord->allita_id);
+                                $allitaTableRecord = UnitAmenity::find($updateRecord->allita_id);
                                 /// END NEW CODE PART 1
 
                                 // convert dates to seconds and miliseconds to see if the current record is newer.
@@ -108,13 +108,14 @@ class SyncController extends Controller
 
                                         // record is newer than the one currently on file in the allita db.
                                         // update the sync table first
-                                        SyncMonitoringMonitor::where('id',$updateRecord['id'])
+                                        SyncUnitAmenity::where('id',$updateRecord['id'])
                                         ->update([
                                             
                                             
                                             
-                                            'monitoring_key'=>$v['attributes']['monitoringKey'],
-                                            'user_key'=>$v['attributes']['userKey'],
+                                            'unit_key'=>$v['attributes']['unitKey'],
+                                            'amenity_type_key'=>$v['attributes']['amenityTypeKey'],
+                                            'comment'=>$v['attributes']['comment'],
                                             
                                             
                                             
@@ -123,14 +124,15 @@ class SyncController extends Controller
                                             
                                             'last_edited'=>$v['attributes']['lastEdited'],
                                         ]);
-                                        $UpdateAllitaValues = SyncMonitoringMonitor::find($updateRecord['id']);
+                                        $UpdateAllitaValues = SyncUnitAmenity::find($updateRecord['id']);
                                         // update the allita db - we use the updated at of the sync table as the last edited value for the actual Allita Table.
                                         $allitaTableRecord->update([
                                             
                                             
                                             
-                                            'monitoring_key'=>$v['attributes']['monitoringKey'],
-                                            'user_key'=>$v['attributes']['userKey'],
+                                            'unit_key'=>$v['attributes']['unitKey'],
+                                            'amenity_type_key'=>$v['attributes']['amenityTypeKey'],
+                                            'comment'=>$v['attributes']['comment'],
                                             
                                             
                                             
@@ -147,37 +149,39 @@ class SyncController extends Controller
                                         // date ends up in the allita table record
                                         // (if we create the sync record first the updated at date would become out of sync with the allita table.)
 
-                                        $allitaTableRecord = AuditAuditor::create([
+                                        $allitaTableRecord = UnitAmenity::create([
                                             
                                             
                                             
                                             
-                                            'monitoring_key'=>$v['attributes']['monitoringKey'],
-                                            'user_key'=>$v['attributes']['userKey'],
+                                            'unit_key'=>$v['attributes']['unitKey'],
+                                            'amenity_type_key'=>$v['attributes']['amenityTypeKey'],
+                                            'comment'=>$v['attributes']['comment'],
                                             
                                             
                                             
                                             
                                             
                                             
-                                            'monitoring_monitor_key'=>$v['attributes']['monitoringMonitorKey'],
+                                            'unit_amenity_key'=>$v['attributes']['unitAmenityKey'],
                                         ]);
                                         // Create the sync table entry with the allita id
-                                        $syncTableRecord = SyncMonitoringMonitor::where('id',$updateRecord['id'])
+                                        $syncTableRecord = SyncUnitAmenity::where('id',$updateRecord['id'])
                                         ->update([
                                             
                                             
                                             
                                             
-                                            'monitoring_key'=>$v['attributes']['monitoringKey'],
-                                            'user_key'=>$v['attributes']['userKey'],
+                                            'unit_key'=>$v['attributes']['unitKey'],
+                                            'amenity_type_key'=>$v['attributes']['amenityTypeKey'],
+                                            'comment'=>$v['attributes']['comment'],
                                             
                                             
                                             
                                             
                                             
                                             
-                                            'monitoring_monitor_key'=>$v['attributes']['monitoringMonitorKey'],
+                                            'unit_amenity_key'=>$v['attributes']['unitAmenityKey'],
                                             'last_edited'=>$v['attributes']['lastEdited'],
                                             'allita_id'=>$allitaTableRecord->id,
                                         ]);                                     
@@ -193,36 +197,38 @@ class SyncController extends Controller
                                 // Create the Allita Entry First
                                 // We do this so the updated_at value of the Sync Table does not become newer
                                 // when we add in the allita_id
-                                $allitaTableRecord = AuditAuditor::create([
+                                $allitaTableRecord = UnitAmenity::create([
                                     
 
                                             
-                                            'monitoring_monitor_key'=>$v['attributes']['monitoringMonitorKey'],
-                                            'monitoring_key'=>$v['attributes']['monitoringKey'],
-                                            'user_key'=>$v['attributes']['userKey'],
+                                            'unit_amenity_key'=>$v['attributes']['unitAmenityKey'],
+                                            'unit_key'=>$v['attributes']['unitKey'],
+                                            'amenity_type_key'=>$v['attributes']['amenityTypeKey'],
+                                            'comment'=>$v['attributes']['comment'],
                                             
                                             
                                             
                                             
                                             
                                     
-                                    'monitoring_monitor_key'=>$v['attributes']['monitoringMonitorKey'],
+                                    'unit_amenity_key'=>$v['attributes']['unitAmenityKey'],
                                 ]);
                                 // Create the sync table entry with the allita id
-                                $syncTableRecord = SyncMonitoringMonitor::create([
+                                $syncTableRecord = SyncUnitAmenity::create([
                                             
                                             
                                             
                                             
-                                            'monitoring_key'=>$v['attributes']['monitoringKey'],
-                                            'user_key'=>$v['attributes']['userKey'],
+                                            'unit_key'=>$v['attributes']['unitKey'],
+                                            'amenity_type_key'=>$v['attributes']['amenityTypeKey'],
+                                            'comment'=>$v['attributes']['comment'],
                                             
                                             
                                             
                                             
                                             
 
-                                        'monitoring_monitor_key'=>$v['attributes']['monitoringMonitorKey'],
+                                        'unit_amenity_key'=>$v['attributes']['unitAmenityKey'],
                                         'last_edited'=>$v['attributes']['lastEdited'],
                                         'allita_id'=>$allitaTableRecord->id,
                                 ]);
