b1b4b51c261cfd94d17b5681ec64d1af582e2b3a
marcel-krsh <marcel.krsh@gmail.com>
2018-12-12 19:11:45 -0500

    fix the model

--- app/Models/SyncUser.php
+++ app/Models/SyncUser.php
@@ -3,13 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\SystemSetting;
+//use App\Models\SystemSetting;
 
 class SyncUser extends Model
 {
     public $timestamps = true;
 	//protected $dateFormat = 'Y-m-d\TH:i:s.u';
-	public $ohfa_id = SystemSetting::get('ohfa_organization_id');
+	
 
 	
 
