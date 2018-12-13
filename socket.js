f2a388a0bc420a935e0731b60f2e2fc0d527a316
marcel-krsh <marcel.krsh@gmail.com>
2018-12-13 11:19:31 -0500

    socket

--- socket.js
+++ socket.js
@@ -34,8 +34,8 @@ redis.on('message', function(channel, message) {
 });
 
 
-server.listen(443, function(){
-    console.log('Listening on Port 443');
+server.listen(3000, function(){
+    console.log('Listening on Port 3000');
 });
 
 redis.on("error", function (err) {
