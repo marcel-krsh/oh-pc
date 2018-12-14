8190b94d1333f8dec368b79455e6db915bc393b4
marcel-krsh <marcel.krsh@gmail.com>
2018-12-14 12:09:29 -0500

    unit

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -1909,6 +1909,6 @@ class DevcoService extends PCAPIService
 
 		$log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-		return $this->get("devco/unit_programs?{$params}&{$log_params}");
+		return $this->get("devco/unit_programs/{$unitId}?{$log_params}");
 	}
 }
