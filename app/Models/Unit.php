766dde9cbde319bb8d64e95b0ef8dc94f3d3c632
marcel-krsh <marcel.krsh@gmail.com>
2018-12-14 10:41:33 -0500

    unit model

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Unit extends Model
 {
@@ -10,4 +12,24 @@ class Unit extends Model
 	//protected $dateFormat = 'Y-m-d\TH:i:s.u';
     //
     protected $guarded = ['id'];
+
+    public function household() : HasOne
+    {
+        return $this->hasOne(\App\Models\Household::class, 'unit_key', 'unit_key');
+    }
+
+    public function household_events() : HasMany
+    {
+    	return $this->hasMany(\App\Models\HouseholdEvent::class, 'unit_key', 'unit_key');
+    }
+
+    public function isAssistedUnit() : bool
+    {
+    	foreach ($this->household_events()->get() as $event) {
+            if ($event->rental_assistance_amount > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
