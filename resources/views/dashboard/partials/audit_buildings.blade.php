17d60925a7d68a8d4dcbc793b596a43b2ee0c928
marcel-krsh <marcel.krsh@gmail.com>
2018-12-16 00:10:29 -0500

    no message

--- resources/views/dashboard/partials/audit_buildings.blade.php
+++ resources/views/dashboard/partials/audit_buildings.blade.php
@@ -61,8 +61,7 @@
 															<div class="building-status">
 																
 																<span class="uk-badge colored" uk-tooltip="pos:top-left;title:# finding icon;" title="@if(!is_null($building->building->findings_json))
-																	<?php dd($building->building->findings_json); ?>
-																	forEach($building->building->findings_json as $finding){{strtoupper($finding->finding_type)}}: {{$finding->finding_description}}<br/>endForEach @else No Findings @endIf">{{$building->building->finding_total}}</span>
+																	@forEach($building->building->findings_json as $finding) {{strtoupper($finding->finding_type)}}: {{$finding->finding_description}}<br/> @endForEach @else No Findings @endIf">{{$building->building->finding_total}}</span>
 																}
 															</div>
 														</div>
