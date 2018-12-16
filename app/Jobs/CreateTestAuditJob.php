ef331ce57ca4b773d8a45ba1fc047d43504227f2
marcel-krsh <marcel.krsh@gmail.com>
2018-12-16 12:54:36 -0500

    Make it a job

---
+++ app/Jobs/CreateTestAuditJob.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Services\AuthService;
+use App\Services\DevcoService;
+use App\Models\AuthTracker;
+use App\Models\SystemSetting;
+use App\Models\User;
+use DB;
+use DateTime;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\SyncAddress;
+use App\Models\Address;
+
+class CreateTestAuditJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    public $tries = 5;
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Event::fire('audit.created', $testaudit);
+    }
+}
