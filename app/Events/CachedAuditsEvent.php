917369190ed9b58d4e933cc8ca08598e2fd95675
marcel-krsh <marcel.krsh@gmail.com>
2018-12-16 16:55:27 -0500

    fixed unit inspected duplicates

--- app/Events/CachedAuditsEvent.php
+++ app/Events/CachedAuditsEvent.php
@@ -21,6 +21,7 @@ use App\Models\BuildingInspection;
 use App\Models\ProjectContactRole;
 use App\Models\CachedAudit;
 use App\Models\CachedBuilding;
+use App\Models\CachedUnit;
 use App\Models\AuditAuditor;
 use App\Models\Program;
 use App\Services\DevcoService;
@@ -103,8 +104,6 @@ class CachedAuditsEvent
         foreach($buildings as $building){
             $count_units = UnitInspection::where('building_key', '=', $building->building_key)->count();
             $finding_total = $building->nlt_count + $building->lt_count + $building->file_count;
-
-            
             
 
             $cached_building = new CachedBuilding([
@@ -138,6 +137,53 @@ class CachedAuditsEvent
                 'auditors_json' => json_encode($auditors_array)
             ]);
             $cached_building->save();   
+        }
+
+        // create cached units
+        $units = UnitInspection::where('audit_key', '=', $cached_audit->audit_key)->get();
+
+        foreach($units as $unit){
+
+            // get the unit type (bedroom type)
+            // 
+            // 
+
+            // $cached_unit = new CachedUnit([
+            //     'audit_id' => $cached_audit->audit_id,
+            //     'audit_key' => $cached_audit->audit_key,
+            //     'project_id' => $building->project_id,
+            //     'project_key' => $building->project_key,
+            //     'amenity_id' => null,
+            //     'building_id' => $building->building_id,
+            //     'building_key' => $building->building_key,
+            //     'status' => null,
+            //     'type' => ,
+            //     'type_total',
+            //     'type_text',
+            //     'type_text_plural',
+            //     'program_total',
+            //     'finding_total',
+            //     'finding_file_status',
+            //     'finding_nlt_status',
+            //     'finding_lt_status',
+            //     'finding_sd_status',
+            //     'finding_file_total',
+            //     'finding_nlt_total',
+            //     'finding_lt_total',
+            //     'finding_sd_total',
+            //     'finding_file_completed',
+            //     'finding_nlt_completed',
+            //     'finding_lt_completed',
+            //     'finding_sd_completed',
+            //     'followup_date',
+            //     'address',
+            //     'city',
+            //     'state',
+            //     'zip',
+            //     'auditors_json',
+            //     'amenities_json',
+            // ]);
+            // $cached_unit->save();   
         }
         
     }
