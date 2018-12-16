6adc57bd3d74b5c40441e04a8f2b3d74e6cd6ce8
marcel-krsh <marcel.krsh@gmail.com>
2018-12-16 10:47:44 -0500

    updates to user experience

--- resources/views/dashboard/reports.blade.php
+++ resources/views/dashboard/reports.blade.php
@@ -1,1 +1,6 @@
-REPORTS
+No reports in system currently.
+<?php // keep this script at the bottom of page to ensure the tabs behave appropriately ?>
+<script>
+	window.reportsLoaded = 1;
+</script>
+<?php // end script keep ?>
