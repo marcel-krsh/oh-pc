1380fd2af86af0a891f8971ffe5c231fea9cad99
marcel-krsh <marcel.krsh@gmail.com>
2018-12-16 14:23:36 -0500

    push

--- app/Events/CachedAuditsEvent.php
+++ app/Events/CachedAuditsEvent.php
@@ -26,6 +26,7 @@ use App\Services\DevcoService;
 use App\Models\UnitProgram;
 use Illuminate\Support\Facades\Redis;
 use Auth;
+use Log;
 
 class CachedAuditsEvent
 {
@@ -59,42 +60,49 @@ class CachedAuditsEvent
     	// create buildings, create units, create amenities cache tables
 
     	// get buildings from cached_audit
-    	$buildings = BuildingInspection::where('audit_id','=',$cached_audit->audit_id)->get();
+        try{
+        	$buildings = BuildingInspection::where('audit_id','=',$cached_audit->audit_id)->get();
 
-    	// create cached buildings related to this audit
-    	foreach($buildings as $building){
-    		$count_units = UnitInspection::where('building_key', '=', $building->building_key)->count();
-    		$finding_total = $building->nlt_count + $building->lt_count + $building->file_count;
+        	// create cached buildings related to this audit
+        	foreach($buildings as $building){
+                try{
+            		$count_units = UnitInspection::where('building_key', '=', $building->building_key)->count();
+            		$finding_total = $building->nlt_count + $building->lt_count + $building->file_count;
 
-    		$cached_building = new CachedBuilding([
-                'building_id' => $building->building_id,
-                'building_key' => $building->building_key,
-                'audit_id' => $cached_audit->audit_id,
-                'audit_key' => $cached_audit->audit_key,
-                'project_id' => $building->project_id,
-                'project_key' => $building->project_key,
-                'status' => '',
-                'type' => 'unit', 
-                'type_total' => $count_units,
-                'type_text' => 'UNIT',
-                'type_text_plural' => 'UNITS',
-                'finding_total' => $finding_total,
-                'finding_file_status' => '',
-                'finding_nlt_status' => '',
-                'finding_lt_status' => '',
-                'finding_file_total' => $building->file_count,
-                'finding_file_completed' => 0,
-                'finding_nlt_total' => $building->nlt_count,
-                'finding_nlt_completed' => 0,
-                'finding_lt_total' => $building->lt_count,
-                'finding_lt_completed' => 0,
-                'address' => $cached_audit->address,
-		        'city' => $cached_audit->city,
-		        'state' => $cached_audit->state,
-		        'zip' => $cached_audit->zip
-            ]);
-            $cached_building->save();	
-    	}
-    	
+            		$cached_building = new CachedBuilding([
+                        'building_id' => $building->building_id,
+                        'building_key' => $building->building_key,
+                        'audit_id' => $cached_audit->audit_id,
+                        'audit_key' => $cached_audit->audit_key,
+                        'project_id' => $building->project_id,
+                        'project_key' => $building->project_key,
+                        'status' => '',
+                        'type' => 'unit', 
+                        'type_total' => $count_units,
+                        'type_text' => 'UNIT',
+                        'type_text_plural' => 'UNITS',
+                        'finding_total' => $finding_total,
+                        'finding_file_status' => '',
+                        'finding_nlt_status' => '',
+                        'finding_lt_status' => '',
+                        'finding_file_total' => $building->file_count,
+                        'finding_file_completed' => 0,
+                        'finding_nlt_total' => $building->nlt_count,
+                        'finding_nlt_completed' => 0,
+                        'finding_lt_total' => $building->lt_count,
+                        'finding_lt_completed' => 0,
+                        'address' => $cached_audit->address,
+        		        'city' => $cached_audit->city,
+        		        'state' => $cached_audit->state,
+        		        'zip' => $cached_audit->zip
+                    ]);
+                    $cached_building->save();	
+        	   }catch(Exception $e){
+                    Log::info('Failed creating building cache record:'.$e);
+               }
+            }
+        }
+    } catch(Exception $e){ 
+        Log::info('Failed creating audit cache:'.$e);
     }
 }
