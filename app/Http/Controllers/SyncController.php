14ff6ce23dac8a01400013cad3e19dda8b84675d
marcel-krsh <marcel.krsh@gmail.com>
2019-01-07 11:04:31 -0500

    Make it the object

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -371,7 +371,7 @@ class SyncController extends Controller
         if(!is_null($model) && strtoupper($model) !== 'NULL'){
             if(!is_null($referenceId) && strtoupper($referenceId) !== 'NULL'){
                 $model = 'App\\Models\\'.$model;
-                $model::find($referenceId);
+                $model = $model::find($referenceId);
                 dd($model);  
             } else {
                 return "Sync Devco: No reference id specified.";
