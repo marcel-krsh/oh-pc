72878fe2574132a9c1719fd065afacc0a2cd05a9
marcel-krsh <marcel.krsh@gmail.com>
2019-01-07 19:20:57 -0500

    add back in the vars

--- routes/channels.php
+++ routes/channels.php
@@ -30,6 +30,6 @@ Broadcast::channel('communications.{uid}.{sid}', function ($user, $uid, $sid) {
 //   return true; // return Auth::check();
 // });
 
-Broadcast::channel('chat', function () {
+Broadcast::channel('chat.{uid}.{sid}', function () {
   return true; // return Auth::check();
 });
