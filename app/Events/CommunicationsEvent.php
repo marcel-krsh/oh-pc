598a9a431b01b0775f83bfe45c81db54cdfe04a4
marcel-krsh <marcel.krsh@gmail.com>
2019-01-09 10:52:12 -0500

    Fix reference to the event

--- app/Events/CommunicationsEvent.php
+++ app/Events/CommunicationsEvent.php
@@ -2,7 +2,7 @@
 
 namespace App\Events;
 
-use App\Events\CommunicationBroadcastEvent;
+use App\Events\UpdateEvent;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
