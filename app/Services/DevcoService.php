394b26d828e6fcb16156283d990e44a6342098b8
marcel-krsh <marcel.krsh@gmail.com>
2019-01-12 22:40:51 -0500

    ugh

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -1933,7 +1933,8 @@ class DevcoService extends PCAPIService
         }
 
 
-        $log_params = "cabinet={$cabinetNumber}&user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
+        $log_params = "cabinet={$cabinetNumber}";
+        //&user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
         return $this->get("docuware/documents/search?{$log_params}&search={$search}");
     }
