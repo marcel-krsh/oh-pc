38704aea390b6f49ac8610b7aff54b054cc45e93
marcel-krsh <marcel.krsh@gmail.com>
2019-01-13 15:57:40 -0500

    test targeting

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -25,7 +25,7 @@ class SyncController extends Controller
     public function getDocs(string $projectNumber, string $searchString = null, int $deviceId=0 , string $deviceName='System'){
         $apiConnect = new DevcoService();
         $documentList = $apiConnect->getProjectDocuments($projectNumber, $searchString, Auth::user()->id, Auth::user()->email, Auth::user()->name, $deviceId, $deviceName);
-        dd($documentList);
+        dd($documentList,$documentList->included[0]['id']);
 
     }
 
