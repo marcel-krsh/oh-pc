4a8d806d08a2863a3a6adab879b610918288a67b
marcel-krsh <marcel.krsh@gmail.com>
2019-01-14 15:44:47 -0500

    figuring out the bracket missing

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -134,7 +134,7 @@ class DocumentController extends Controller
                                     ]);
                                 }
                                 
-                           
+                        }
 
 
 
