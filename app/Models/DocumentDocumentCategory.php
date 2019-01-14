011651ce0d9605b69bb6fe0f91e93b38777b1a19
marcel-krsh <marcel.krsh@gmail.com>
2019-01-14 14:03:51 -0500

    only guard the id

--- app/Models/DocumentDocumentCategory.php
+++ app/Models/DocumentDocumentCategory.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class DocumentDocumentCategory extends Model
 {
-    //
+    public $timestamps = true;
+    //protected $dateFormat = 'Y-m-d G:i:s.u';
+    
+    protected $guarded = ['id'];
 }
