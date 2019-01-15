8e6958155b826fefa2030e44ebe27b8072cfb754
marcel-krsh <marcel.krsh@gmail.com>
2019-01-14 22:30:18 -0500

    add some class

--- resources/views/projects/partials/documents.blade.php
+++ resources/views/projects/partials/documents.blade.php
@@ -53,7 +53,7 @@
         </thead>
         <tbody id="sent-document-list">
             @foreach ($documents as $document)
-        <tr>
+        <tr class="{{strtolower(strreplace(' ','-',$document->document_class))}} {{strreplace(' ','-',strtolower($document->document_description))}}">
             
             <td>
                 {{ucwords(strtolower($document->document_class))}}
