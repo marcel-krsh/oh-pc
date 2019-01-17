5ff9984cc5225b192cf66abe8a2db6b2c6d15cb4
marcel-krsh <marcel.krsh@gmail.com>
2019-01-16 19:28:15 -0500

    Fix NA

--- resources/views/admin_tabs/users.blade.php
+++ resources/views/admin_tabs/users.blade.php
@@ -48,7 +48,7 @@
                                 {!!$user->organization_details->address->formatted_address()!!}
                         </div>
                         
-                        @else NA @endif
+                        @else <i class="a-marker-basic uk-text-muted uk-link"></i>NA @endif
                     </small></td>
                     <td><small>@if($user->has_organization()) {{$user->organization_details->phone_number_formatted()}} @endif</small></td>
                     <td><small><a href="mailto:{{$user->person->email->email_address}}">{{$user->person->email->email_address}}</a></small></td>
