01aa73d771c1bd974112c349ea2cea47c41c7e1b
marcel-krsh <marcel.krsh@gmail.com>
2019-01-17 14:31:16 -0500

    add has many and one to models

--- app/Models/BuildingInspection.php
+++ app/Models/BuildingInspection.php
@@ -4,17 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class BuildingInspection extends Model
 {
     public $timestamps = true;
     //protected $dateFormat = 'Y-m-d\TH:i:s.u';
 
-    public function amenities()
+    public function amenities() : HasMany
     {
         return $this->hasMany(\App\Models\BuildingAmenity::class, 'building_id', 'building_id');
     }
 
-    public function building()
+    public function building() : HasOne
     {
     	return $this->hasOne(\App\Models\Building::class, 'id', 'building_id');
     }
--- app/Models/UnitInspection.php
+++ app/Models/UnitInspection.php
@@ -4,18 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class UnitInspection extends Model
 {
     public $timestamps = true;
     //protected $dateFormat = 'Y-m-d\TH:i:s.u';
 
     
-    public function amenities()
+    public function amenities() : HasMany
     {
         return $this->hasMany(\App\Models\UnitAmenity::class, 'unit_id', 'unit_id');
     }
 
-    public function unit()
+    public function unit() : HasOne
     {
         return $this->hasOne(\App\Models\Unit::class, 'id', 'unit_id');
     }
