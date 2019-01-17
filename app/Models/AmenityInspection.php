945af693d7b6813b65e222f9ff80966b5d0c2e2f
marcel-krsh <marcel.krsh@gmail.com>
2019-01-17 14:55:13 -0500

    Silly rabbit - use is for good things

--- app/Models/AmenityInspection.php
+++ app/Models/AmenityInspection.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class AmenityInspection extends Model
 {
     public $timestamps = true;
