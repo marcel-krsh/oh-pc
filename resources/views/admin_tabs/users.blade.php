20b256a8f06effa07036a4e50218bfd9af6baa97
marcel-krsh <marcel.krsh@gmail.com>
2019-01-16 19:25:55 -0500

    Align left

--- resources/views/admin_tabs/users.blade.php
+++ resources/views/admin_tabs/users.blade.php
@@ -40,7 +40,15 @@
                     
                     <td><small>@if($user->has_organization())
                         {{$user->organization_details->organization_name}}@else NA @endif</small></td>
-                    <td><small>@if($user->has_address())<div class="uk-align-right">{!!$user->organization_details->address->formatted_address()!!}</div><a target="_blank" href="https://www.google.com/maps?q={{$user->organization_details->address->formatted_address()}}" class="uk-text-muted uk-align-right"><i class="a-marker-basic uk-text-muted uk-link"></i></a> @else NA @endif</small></td>
+                    <td><small>@if($user->has_address())
+                        <div class="uk-align-left">
+                                {!!$user->organization_details->address->formatted_address()!!}
+                        </div>
+                        <a target="_blank" href="https://www.google.com/maps?q={{$user->organization_details->address->formatted_address()}}" class="uk-text-muted uk-align-left">
+                            <i class="a-marker-basic uk-text-muted uk-link"></i>
+                        </a> 
+                        @else NA @endif
+                    </small></td>
                     <td><small>@if($user->has_organization()) {{$user->organization_details->phone_number_formatted()}} @endif</small></td>
                     <td><small><a href="mailto:{{$user->person->email->email_address}}">{{$user->person->email->email_address}}</a></small></td>
                     <td class="use-hand-cursor" uk-tooltip="title:CLICK TO SET ROLES" onclick="setRoles({{$user->id}})"><small>@if($user->roles_list() != ''){{$user->roles_list()}}@else <i class="a-circle-plus"></i>@endif</small></td>
