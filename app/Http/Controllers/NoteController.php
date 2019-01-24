e018017f495dd0c78fec7cc69b38118638f90d29
marcel-krsh <marcel.krsh@gmail.com>
2019-01-24 11:16:07 -0800

    test

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -53,9 +53,8 @@ class NoteController extends Controller
         }
 
        // $units = UnitInspection::where('audit_key', '=', '5974')->with('unit','unit.building.address')->take(10)->get();
-        $units = UnitInspection::where('audit_key', '=', '5974')->selectRaw('unit_id, ANY_VALUE(audit_key), ANY_VALUE(project_id), ANY_VALUE(project_key), ANY_VALUE(building_id), ANY_VALUE(building_key)')->groupBy('unit_id', 'audit_key', 'project_id', 'project_key', 'building_id', 'building_key')->take(10)->get();
+        $units = UnitInspection::where('audit_key', '=', '5974')->selectRaw('unit_id, ANY_VALUE(audit_key) as audit_key, ANY_VALUE(project_id) as project_id, ANY_VALUE(project_key) as project_key, ANY_VALUE(building_id) as building_id, ANY_VALUE(building_key) as building_key')->groupBy('unit_id', 'audit_key', 'project_id', 'project_key', 'building_id', 'building_key')->take(10)->get();
         
-        dd($units);
         $units_added = array();
 
         foreach ($units as $unit) {
