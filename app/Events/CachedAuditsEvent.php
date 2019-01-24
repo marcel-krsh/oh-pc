6f8e37081734249aae41f2c55504859b798e034f
marcel-krsh <marcel.krsh@gmail.com>
2019-01-23 23:07:36 -0800

    let's try that again

--- app/Events/CachedAuditsEvent.php
+++ app/Events/CachedAuditsEvent.php
@@ -226,72 +226,78 @@ class CachedAuditsEvent
 
         CachedUnit::where('audit_id',$cached_audit->audit_id)->delete();
         
+        $units_added = array();
+
         foreach ($units as $unit) {
-            // get the unit type (bedroom type)
-            //
-            //
-            $unit_amenities = AmenityInspection::where('unit_id',$unit->unit_id)->with('amenity')->get();
-            //dd($unit_amenities);
-            $uaCount = 0;
-            //Unit amenity json:
-            //[{"id": "295", "qty": "2", "type": "Elevator", "status": "pending"},]
-            $uaJson = '[';
-            forEach($unit_amenities as $ua){
-                if($ua->amenity->inspectable){
-                    $uaCount++;
-                    if($jsonRun == 1){
-                        $uaJson .= ' , ';
-                        //insert comma between groups
+            if(!in_array($unit->unit_id, $units_added)){
+                $units_added[] = $unit->unit_id;
+                
+                // get the unit type (bedroom type)
+                //
+                //
+                $unit_amenities = AmenityInspection::where('unit_id',$unit->unit_id)->with('amenity')->get();
+                //dd($unit_amenities);
+                $uaCount = 0;
+                //Unit amenity json:
+                //[{"id": "295", "qty": "2", "type": "Elevator", "status": "pending"},]
+                $uaJson = '[';
+                forEach($unit_amenities as $ua){
+                    if($ua->amenity->inspectable){
+                        $uaCount++;
+                        if($jsonRun == 1){
+                            $uaJson .= ' , ';
+                            //insert comma between groups
+                        }
+                        $jsonRun = 1;
+
+                        $uaJson .= '{"id": "'.$ua->amenity_id.'", "qty": "0", "type": "'.addslashes($ua->amenity->amenity_description).'","status":"","common_area":"'.$ua->common_area.'","project":"'.$ua->project.'","building_system":"'.$ua->building_system.'","building_exterior":"'.$ua->building_exterior.'","unit":"'.$ua->unit.'","file":"'.$ua->file.'"}';
+                        
                     }
-                    $jsonRun = 1;
-
-                    $uaJson .= '{"id": "'.$ua->amenity_id.'", "qty": "0", "type": "'.addslashes($ua->amenity->amenity_description).'","status":"","common_area":"'.$ua->common_area.'","project":"'.$ua->project.'","building_system":"'.$ua->building_system.'","building_exterior":"'.$ua->building_exterior.'","unit":"'.$ua->unit.'","file":"'.$ua->file.'"}';
-                    
                 }
-            }
-            $uaJson .= ']';
-            $jsonRun = 0;
-            
+                $uaJson .= ']';
+                $jsonRun = 0;
+                
 
-            $cached_unit = new CachedUnit([
-                'audit_id' => $cached_audit->audit_id,
-                'audit_key' => $cached_audit->audit_key,
-                'project_id' => $unit->project_id,
-                'project_key' => $unit->project_key,
-                'amenity_id' => null,
-                'building_id' => $unit->building_id,
-                'building_key' => $unit->building_key,
-                'status' => null,
-                'type' => 'amenity',
-                'type_total' => $uaCount,
-                'type_text' => 'AMENITY',
-                'type_text_plural' => 'AMENITIES',
-                'program_total' => null,
-                'finding_total' => 0,
-                'finding_file_status' => '',
-                'finding_nlt_status' => '',
-                'finding_lt_status' => '',
-                'finding_sd_status' => '',
-                'finding_file_total' => '0',
-                'finding_nlt_total' => '0',
-                'finding_lt_total' => '0',
-                'finding_sd_total' => '0',
-                'finding_file_completed' => '0',
-                'finding_nlt_completed' => '0',
-                'finding_lt_completed' => '0',
-                'finding_sd_completed' => '0',
-              //  'followup_date' => '',
-                'address' => $unit->unit->building->address->line_1,
-                'city' => $unit->unit->building->address->city,
-                'state' => $unit->unit->building->address->state,
-                'zip' => $unit->unit->building->address->zip,
-                'auditors_json' => null,
-                'amenities_json' => $uaJson,
-                'unit_id'=>$unit->unit->id,
-                'unit_key'=>$unit->unit->unit_key,
-                'unit_name'=>$unit->unit->unit_name
-            ]);
-            $cached_unit->save();
+                $cached_unit = new CachedUnit([
+                    'audit_id' => $cached_audit->audit_id,
+                    'audit_key' => $cached_audit->audit_key,
+                    'project_id' => $unit->project_id,
+                    'project_key' => $unit->project_key,
+                    'amenity_id' => null,
+                    'building_id' => $unit->building_id,
+                    'building_key' => $unit->building_key,
+                    'status' => null,
+                    'type' => 'amenity',
+                    'type_total' => $uaCount,
+                    'type_text' => 'AMENITY',
+                    'type_text_plural' => 'AMENITIES',
+                    'program_total' => null,
+                    'finding_total' => 0,
+                    'finding_file_status' => '',
+                    'finding_nlt_status' => '',
+                    'finding_lt_status' => '',
+                    'finding_sd_status' => '',
+                    'finding_file_total' => '0',
+                    'finding_nlt_total' => '0',
+                    'finding_lt_total' => '0',
+                    'finding_sd_total' => '0',
+                    'finding_file_completed' => '0',
+                    'finding_nlt_completed' => '0',
+                    'finding_lt_completed' => '0',
+                    'finding_sd_completed' => '0',
+                  //  'followup_date' => '',
+                    'address' => $unit->unit->building->address->line_1,
+                    'city' => $unit->unit->building->address->city,
+                    'state' => $unit->unit->building->address->state,
+                    'zip' => $unit->unit->building->address->zip,
+                    'auditors_json' => null,
+                    'amenities_json' => $uaJson,
+                    'unit_id'=>$unit->unit->id,
+                    'unit_key'=>$unit->unit->unit_key,
+                    'unit_name'=>$unit->unit->unit_name
+                ]);
+                $cached_unit->save();
+            }
         }
     }
 }
