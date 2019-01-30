3df0d5a59470452d52f021fd2aa9abd904935631
marcel-krsh <marcel.krsh@gmail.com>
2019-01-29 19:02:08 -0500

    Add in the use

--- app/Models/SyncDocuware.php
+++ app/Models/SyncDocuware.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class SyncDocuware extends Model
 {
