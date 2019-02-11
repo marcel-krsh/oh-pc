3a0e9e174fc5db62562617939da5e2ab1cff216f
marcel-krsh <marcel.krsh@gmail.com>
2019-02-11 00:08:16 -0500

    blah

--- app/Models/Finding.php
+++ app/Models/Finding.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Event;
 
 class Finding extends Model
 {
