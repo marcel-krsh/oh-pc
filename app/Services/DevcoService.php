10631c057745f12469625b1a55703d00b8fc3aa2
marcel-krsh <marcel.krsh@gmail.com>
2019-02-16 23:05:08 -0500

    send it both in the get and the post to make sure it works

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -1926,12 +1926,13 @@ class DevcoService extends PCAPIService
      */
 
     public function putUnitProgram($unitKey, $programKey, $fundingProgramKey,$startDate,$endDate, int $user = null, string $user_email = null, string $user_name = null, int $device_id = null, string $device_name = null) {
-
+        $data = array();
         $params = "unitKey={$unitKey}&programKey={$programKey}&fundingProgramKey={$fundingProgramKey}&startDate={$startDate}&endDate={$endDate}";
+        $data = ['unitKey'=>$unitKey,'programKey'=>$programKey, 'fundingProgramKey'=>$fundingProgramKey,'startDate'=>$startDate,'endDate'=>$endDate];
 
         $log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-        return $this->put("devco/unit_development_programs?{$params}&{$log_params}");
+        return $this->put("devco/unit_development_programs?{$params}&{$log_params}",$data);
     }
 
     public function getProjectDocuments(string $projectNumber = '1', string $searchString = null, int $user = null, string $user_email = null, string $user_name = null, int $device_id = null, string $device_name = null)
