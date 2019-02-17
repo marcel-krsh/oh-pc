c738963183ad754ffca8c1ce229baceb26865079
marcel-krsh <marcel.krsh@gmail.com>
2019-02-17 18:54:37 -0500

    try with this

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -1934,7 +1934,7 @@ class DevcoService extends PCAPIService
         $log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
         return $this->put("devco/unit_development_programs?{$log_params}",[
-    GuzzleHttp\RequestOptions::JSON => $data]);
+                    $this->JSON => $data]);
     }
 
     public function getProjectDocuments(string $projectNumber = '1', string $searchString = null, int $user = null, string $user_email = null, string $user_name = null, int $device_id = null, string $device_name = null)
