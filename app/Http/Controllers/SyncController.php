7e483daca93694f9ea5c91a766b4cf020ce01a4d
marcel-krsh <marcel.krsh@gmail.com>
2019-02-16 21:36:21 -0500

    get the undefined error... what is happening here...

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -86,7 +86,7 @@ class SyncController extends Controller
 
                         if(is_array($unitPrograms) && count($unitPrograms) > 1){
                             foreach($unitPrograms as $up){
-                                dd($up['attributes']['fundingProgramKey'],$programFundingKeyToProgramKey['key'.$up['attributes']['fundingProgramKey'].'']);
+                                dd($up['attributes']['fundingProgramKey'],$programFundingKeyToProgramKey);
                                 $programKey =  $programFundingKeyToProgramKey['key'.$up['attributes']['fundingProgramKey']];
                                 dd($unit,$up,$unitCount,$canRunCount,$programKey);
                                 // insert the record into the program unit table using the api
