41dbbf37e29ea697ed1c9419750965df157eea07
marcel-krsh <marcel.krsh@gmail.com>
2019-02-17 20:18:31 -0500

    get rid of t in time

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -1928,16 +1928,17 @@ class DevcoService extends PCAPIService
 
     public function putUnitProgram($unitKey, $programKey, $fundingProgramKey,$startDate,$endDate, int $user = null, string $user_email = null, string $user_name = null, int $device_id = null, string $device_name = null) {
         $data = array();
-        if(is_null($endDate)){
-            $endDate = '2015-01-27T00:00:00';
-        }
+        
+            $endDate = '2015-01-27';
+            $startDate = '2015-01-27';
+       
         $data = ['UnitKey'=>$unitKey,'DevelopmentProgramKey'=>$programKey,'StartDate'=>$startDate,'EndDate'=>$endDate];
         json_encode($data);
         //dd($data);
         $log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
 
-        //return $this->post("devco/unit_development_programs?{$log_params}",[\GuzzleHttp\RequestOptions::JSON => ['UnitKey'=>$unitKey,'DevelopmentProgramKey'=>$programKey,'StartDate'=>$startDate,'EndDate'=>$endDate]]);
-        return $this->post("devco/unit_development_programs?{$log_params}",[
+        return $this->post("devco/unit_development_programs?{$log_params}",[\GuzzleHttp\RequestOptions::JSON => ['UnitKey'=>$unitKey,'DevelopmentProgramKey'=>$programKey,'StartDate'=>$startDate,'EndDate'=>$endDate]]);
+        //return $this->post("devco/unit_development_programs?{$log_params}",[
                       'debug' => TRUE,
                       'body' => $data,
                       'headers' => [
