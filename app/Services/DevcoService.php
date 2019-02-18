d75b9ea95da19294922123a4edb2524f841fa65f
marcel-krsh <marcel.krsh@gmail.com>
2019-02-17 21:02:09 -0500

    pushing with standard post method

--- app/Services/DevcoService.php
+++ app/Services/DevcoService.php
@@ -1936,13 +1936,14 @@ class DevcoService extends PCAPIService
         //dd($data);
         $log_params = "user={$user}&user_email={$user_email}&user_name={$user_name}&device_id={$device_id}&device_name={$device_name}";
         
-        return $this->post("devco/unit_development_programs?",[\GuzzleHttp\RequestOptions::JSON => ['UnitKey'=>$unitKey,'DevelopmentProgramKey'=>$programKey,'StartDate'=>$startDate,'EndDate'=>$endDate]]);
+        // return $this->post("devco/unit_development_programs?",[\GuzzleHttp\RequestOptions::JSON => ['UnitKey'=>$unitKey,'DevelopmentProgramKey'=>$programKey,'StartDate'=>$startDate,'EndDate'=>$endDate]]);
         //return $this->post("devco/unit_development_programs?{$log_params}",[
                       // 'debug' => TRUE,
                       // 'body' => $data,
                       // 'headers' => [
                       //   'Content-Type' => 'application/json',]
                       // ]);
+        return $this->post("devco/unit_development_programs?",['UnitKey'=>$unitKey,'DevelopmentProgramKey'=>$programKey,'StartDate'=>$startDate,'EndDate'=>$endDate]);
     }
 
     public function getProjectDocuments(string $projectNumber = '1', string $searchString = null, int $user = null, string $user_email = null, string $user_name = null, int $device_id = null, string $device_name = null)
