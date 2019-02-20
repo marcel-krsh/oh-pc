09c87041b05919dcf6d41d7cf84f50f732a602f8
marcel-krsh <marcel.krsh@gmail.com>
2019-02-20 17:21:18 -0500

    Identify the cancelled programs

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -40,14 +40,20 @@ class SyncController extends Controller
            $unitProjectPrograms = $apiConnect->getUnitProjectPrograms($unit->unit_key, Auth::user()->id, Auth::user()->email, Auth::user()->name, 1, 'SystemServer');
            $projectPrograms = json_decode($unitProjectPrograms);
            $projectPrograms =  $projectPrograms->data;
+           echo "<ul>"
            foreach ($projectPrograms as $pp) {
               $pp = $pp->attributes;
-              echo $pp->unitKey.' '.$pp->developmentProgramKey.' '.$pp->startDate.' '.$pp->endDate.'<br />';
-              //get the matching program from the developmentProgramKey
-              $program = ProjectProgram::where('project_program_key',$pp->developmentProgramKey)->with('program')->first();
-              echo $program->program->program_name.' '.$program->program_id.'<hr ><br />';
+              if(!is_null($pp->endDate)){
+                  echo '<li>'.$pp->unitKey.' '.$pp->developmentProgramKey.' '.$pp->startDate.' '.$pp->endDate.'<br />';
+                  //get the matching program from the developmentProgramKey
+                  $program = ProjectProgram::where('project_program_key',$pp->developmentProgramKey)->with('program')->first();
+                  echo $program->program->program_name.' '.$program->program_id.'</li>';
+                } else {
+                    $program = ProjectProgram::where('project_program_key',$pp->developmentProgramKey)->with('program')->first();
+                        echo "<li>CANCELLED:".$program->program->program_name.' '.$program->program_id.'</li>';
+                }
            }
-           //dd($projectPrograms->data);
+           echo "</ul><hr>"
         }
 
     }
