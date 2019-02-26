b77f8daa42f41f5ff52b53fab99221e34d9869a9
marcel-krsh <marcel.krsh@gmail.com>
2019-02-26 14:08:07 -0500

    one more...

--- resources/views/projects/partials/details.blade.php
+++ resources/views/projects/partials/details.blade.php
@@ -128,7 +128,7 @@
 					<li style="display:none"><span class="indented">Total Building Common Areas</span> <span>{{$details->total_building_common_areas}}</span></li>
 					<li style="display:none"><span class="indented">Total Building Systems</span> <span>{{$details->total_building_systems}}</span></li>
 					<li style="display:none"><span class="indented">Total Building Exteriors</span> <span>{{$details->total_building_exteriors}}</span></li>
-					<li><span>Total Project Common Areas</span> <span></span></li>
+					<li style="display:none"><span>Total Project Common Areas</span> <span></span></li>
 					<li><span>Total Units</span> <span>{{$details->total_units}}</span></li>
 					<li><span class="indented">• Market Rate Units</span> <span>{{$details->market_rate}}</span></li>
 					<li><span class="indented">• Program Units</span> <span>{{$details->subsidized}}</span></li>
