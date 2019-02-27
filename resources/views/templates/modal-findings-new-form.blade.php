19e92c4ecacebfa9930853fedaf5703e6b64b008
marcel-krsh <marcel.krsh@gmail.com>
2019-02-27 16:59:10 -0500

    Test modal close button for use on tablets

--- resources/views/templates/modal-findings-new-form.blade.php
+++ resources/views/templates/modal-findings-new-form.blade.php
@@ -40,7 +40,7 @@
 	    	</div> -->
 	    	
 	    	<div class="uk-width-1-2">
-	    		
+	    		<a class="uk-button" uk-close=""><svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg"><line fill="none" stroke="#000" stroke-width="1.1" x1="1" y1="1" x2="13" y2="13"></line><line fill="none" stroke="#000" stroke-width="1.1" x1="13" y1="1" x2="1" y2="13"></line></svg> CLOSE MODAL</a>
 	    	</div>
 	    	<div class="uk-width-1-2">
 	    		<input type="hidden" id="add-finding-form-finding_date" name="date" value="" />
