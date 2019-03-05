dcef6fb8ab05d2fa355cd846b812a0776ab5f2f7
marcel-krsh <marcel.krsh@gmail.com>
2019-03-05 01:33:45 -0500

    Tweaks

--- resources/views/layouts/allita.blade.php
+++ resources/views/layouts/allita.blade.php
@@ -225,8 +225,8 @@ if(Auth::check()){
 	<a name="top"></a>
 	<div id="phone" class="uk-visible-touch uk-hidden@s">
 		<div id="phone-app" class="uk-container uk-align-center" >
-			<div class="uk-card uk-card-default uk-card-body" style="background:000; margin-bottom: 200px; z-index: 980;" uk-sticky="top: 100; animation: uk-animation-slide-top; bottom: #sticky-on-scroll-up; width-element: #phone">
-					<a ><h2><i class="a-menu uk-text-muted uk-contrast"></i> DEV|CO INSPECT</h2></a>
+			<div class="uk-padding-small" style="background-color:#3c3c3c; margin-bottom: 200px; z-index: 980;" uk-sticky="top: 100; animation: uk-animation-slide-top; bottom: #sticky-on-scroll-up; width-element: #phone">
+					<a class="uk-contrast"><h2><i class="a-menu uk-text-muted uk-contrast"></i> DEV|CO INSPECT</h2></a>
 				</div>
 				<div uk-grid>
 				
