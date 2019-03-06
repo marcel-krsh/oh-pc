bcbc390e1d60aaa63fba67a80f813da7a3a3e552
marcel-krsh <marcel.krsh@gmail.com>
2019-03-06 15:11:21 -0500

    Fix display width issues on table, and make areas clickable that make more natural sense

--- resources/js/components/AuditRow.vue
+++ resources/js/components/AuditRow.vue
@@ -1,6 +1,6 @@
 <template>
 	<tr>
-		<td :id="'audit-c-1-'+audit.auditId" class="uk-text-center audit-td-lead">
+		<td :id="'audit-c-1-'+audit.auditId" class="uk-text-center audit-td-lead use-hand-cursor">
         	<span id="audit-avatar-badge-1" v-on:click="openAssignment" :uk-tooltip="audit.tooltipLead" title="" aria-expanded="false" :class="{[audit.userBadgeColor]:true, 'user-badge':true, 'no-float':true, 'uk-link': true }">
 				<span v-html="audit.initials"></span>
 			</span>
@@ -10,16 +10,16 @@
         	<div class="uk-vertical-align-middle uk-display-inline-block uk-margin-small-top">
         		<span :id="'audit-i-project-detail-'+audit.auditId" v-on:click="openProjectDetails" uk-tooltip="pos:top-left;title:VIEW BUILDINGS AND COMMON AREAS;" class="uk-link"><i class="a-menu uk-text-muted"></i></span>
         	</div>
-        	<div class="uk-vertical-align-middle uk-display-inline-block">
-        		<h3 :id="'audit-project-name-'+audit.auditId" class="uk-margin-bottom-remove uk-link filter-search-project" uk-tooltip="title:OPEN AUDIT DETAILS IN TAB;" v-on:click="openProject"><span v-html="audit.projectRef"></span></h3>
-            	<small :id="'audit-project-aid-'+audit.auditId" class="uk-text-muted faded filter-search-project" uk-tooltip="title:VIEW PROJECT AUDIT DETAILS;">AUDIT <span v-html="audit.auditId"></span></small>
+        	<div class="uk-vertical-align-middle uk-display-inline-block use-hand-cursor"  v-on:click="openProjectDetails" >
+        		<h3 :id="'audit-project-name-'+audit.auditId" class="uk-margin-bottom-remove uk-link filter-search-project" uk-tooltip="title:VIEW BUILDINGS AND COMMON AREAS;"><span v-html="audit.projectRef"></span></h3>
+            	<small :id="'audit-project-aid-'+audit.auditId" class="uk-text-muted faded filter-search-project" uk-tooltip="title:VIEW BUILDINGS AND COMMON AREAS;">AUDIT <span v-html="audit.auditId"></span></small>
             </div>
         </td>
         <td class="audit-td-name">
         	<div class="uk-vertical-align-top uk-display-inline-block uk-margin-small-top uk-margin-small-left">
         		<i class="a-info-circle uk-text-muted uk-link" v-on:click="openContactInfo" uk-tooltip="title:VIEW CONTACT DETAILS;"></i>
         	</div> 
-        	<div class="uk-vertical-align-top uk-display-inline-block fadetext">
+        	<div class="uk-vertical-align-top uk-display-inline-block fadetext use-hand-cursor"  v-on:click="openProject" uk-tooltip="title:VIEW PROJECT DETAILS;">
         		<h3 class="uk-margin-bottom-remove filter-search-pm" v-html="audit.title"></h3>
             	<small class="uk-text-muted faded filter-search-pm" v-html="audit.pm"></small>
         	</div>
@@ -29,7 +29,7 @@
         	<div class="uk-vertical-align-top uk-display-inline-block uk-margin-small-top uk-margin-small-left">
         		<i class="a-marker-basic uk-text-muted uk-link" v-on:click="openMapLink" uk-tooltip="title:VIEW ON MAP;"></i>
         	</div> 
-        	<div class="uk-vertical-align-top uk-display-inline-block fullwidthleftpad fadetext">
+        	<div class="uk-vertical-align-top uk-display-inline-block fullwidthleftpad fadetext" v-on:click="openMapLink">
         		<h3 class="uk-margin-bottom-remove filter-search-address" v-html="audit.address"></h3>
             	<small class="uk-text-muted faded filter-search-address" v-html="audit.address2"></small>
         	</div>
@@ -53,7 +53,7 @@
                 	<div class="uk-width-1-6 uk-text-right uk-padding-remove" uk-tooltip="0 UNITS ASSIGNED TO YOU" v-html="audit.inspectableItems+' /'" v-if="audit.auditor_access && audit.inspectableItems < 1 "></div>
                     <div class="uk-width-1-6 uk-text-right uk-padding-remove" :uk-tooltip="audit.tooltipInspectableItems" v-html="audit.inspectableItems+' /'" v-if="audit.auditor_access && audit.inspectableItems > 0"></div> 
 
-                	<div v-if="audit.auditor_access" class="uk-width-1-6 uk-text-left uk-padding-remove" :uk-tooltip="audit.totalItems + ' TOTAL UNITS WILL BE INSPECTED'" v-html="audit.totalItems"></div> 
+                	<div v-if="audit.auditor_access" class="uk-width-1-6 uk-text-left uk-padding-remove" :uk-tooltip="audit.totalItems + ' TOTAL BUILDINGS PROJECT AMENITIES AND UNITS THAT WILL BE INSPECTED'" v-html="audit.totalItems"></div> 
                 	<div v-if="audit.auditor_access" class="uk-width-1-6 uk-text-left">
                 		<i :class="{[audit.complianceIconClass]:true, [audit.complianceStatusClass]:true}" :uk-tooltip="audit.tooltipComplianceStatus" v-on:click="rerunCompliance"></i>
                 	</div>
@@ -94,18 +94,16 @@
         <td class="hasdivider">
         	<div class="divider"></div>
         	<div class="uk-display-inline-block uk-text-center fullwidth uk-margin-small-top " uk-grid>
-            	<div class="uk-width-1-4">
+            	<div class="uk-width-1-3">
             		<i  v-on:click="openAssignment" :class="{[audit.auditorStatusIconClass]:true, 'use-hand-cursor':true, [audit.auditorStatusClass]:true}" :uk-tooltip="audit.tooltipAuditorStatus"></i>
             	</div> 
-            	<div class="uk-width-1-4">
+            	<div class="uk-width-1-3">
             		<i :class="{[audit.messageStatusIconClass]:true, 'use-hand-cursor':true, [audit.messageStatusClass]:true,}" :uk-tooltip="audit.tooltipMessageStatus"></i>
             	</div> 
-            	<div class="uk-width-1-4">
+            	<div class="uk-width-1-3">
             		<i :class="{[audit.documentStatusIconClass]:true, 'use-hand-cursor':true, [audit.documentStatusClass]:true,}" :uk-tooltip="audit.tooltipDocumentStatus"></i>
             	</div> 
-            	<div class="uk-width-1-4">
-            		<i :class="{[audit.historyStatusIconClass]:true, 'use-hand-cursor':true, [audit.historyStatusClass]:true,}" :uk-tooltip="audit.tooltipHistoryStatus"></i>
-            	</div> 
+            	
             </div>
         </td>
         <td>
