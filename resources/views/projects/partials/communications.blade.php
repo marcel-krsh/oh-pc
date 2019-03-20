6499cf786389ddf5b88f120ee95ee619e605e4a3
marcel-krsh <marcel.krsh@gmail.com>
2019-03-20 07:15:24 +0000

    Main communications and filtering, needs thorough testing

--- resources/views/projects/partials/communications.blade.php
+++ resources/views/projects/partials/communications.blade.php
@@ -1,38 +1,46 @@
-<script>
-    // disable infinite scroll:
-    window.getContentForListId = 0;
-    window.openedCommunication = 0;
-    window.currentCommunication = "";
-    window.restoreLastCommunicationItem = "";
-    window.resetOpenCommunicationId == 0;
-  </script>
-  <div uk-grid class="uk-margin-top" id="message-filters" data-uk-button-radio="">
+<div id="communications_tab">
+	<div uk-grid class="uk-margin-top" id="message-filters" data-uk-button-radio="">
 
-  	<a id="filter-none" class="filter_link" data-filter="all" hidden>all</a>
-  	<a id="filter-attachments" class="filter_link" data-filter="attachment-true" hidden>attachments</a>
+		<a id="filter-none" class="filter_link" data-filter="all" hidden>all</a>
+		<a id="filter-attachments" class="filter_link" data-filter="attachment-true" hidden>attachments</a>
+		{{-- Total of 6 groups
+		* Group 1
+		* 	View inbox
+		* 	View sent messages
+		* Group 2
+		* 	Show results with attachments
+		* 	Show Conversation list view
+		* Group 3
+		* 	Search within message or Audit? Input text field
+		* Group 4
+		* 	Filter by recepient
+		* Group 5
+		* 	Filter by project
+		* Group 6
+		* 	New Message
+		*
+		--}}
 
+		{{-- Group 2, Attachments and conversation list view --}}
+		<div class=" uk-width-1-1@s uk-width-1-5@m">
+			<div uk-grid>
+				<button class="uk-button-large uk-button-default filter-attachments-button uk-width-1-5" uk-tooltip="pos:top-left;title:Show results with attachments">
+					<i class="a-paperclip-2"></i>
+				</button>
+				<input id="communications-project-search" name="communications-project-search" type="text" value="{{ Session::get('communications-search') }}" class="uk-width-4-5 uk-input" placeholder="Search Messages Or Audit ID (press enter)">
+			</div>
+		</div>
 
-
-  	{{-- Group 2, Attachments and conversation list view --}}
-  	<div class=" uk-width-1-1@s uk-width-1-5@m">
-  		<div uk-grid>
-  			<button class="uk-button-large uk-button-default filter-attachments-button uk-width-1-5" uk-tooltip="pos:top-left;title:Show results with attachments">
-  				<i class="a-paperclip-2"></i>
-  			</button>
-  			<input id="communications-search" name="communications-search" type="text" value="{{ Session::get('communications-search') }}" class="uk-width-4-5 uk-input" placeholder="Search Messages Or Audit ID (press enter)">
-  		</div>
-  	</div>
-
-  	<div class="uk-width-1-1@s uk-width-1-5@m" id="recipient-dropdown" style="vertical-align: top;">
-  		<select id="filter-by-owner" class="uk-select filter-drops uk-width-1-1" onchange="filterByOwner();">
-  			<option value="all" selected="">
-  				FILTER BY RECIPIENT
-  			</option>
-  			@foreach ($owners_array as $owner)
-  			<option value="staff-{{$owner['id']}}"><a class="uk-dropdown-close">{{$owner['name']}}</a></option>
-  			@endforeach
-  		</select>
-  	</div>
+		<div class="uk-width-1-1@s uk-width-1-5@m" id="recipient-dropdown" style="vertical-align: top;">
+			<select id="filter-by-owner" class="uk-select filter-drops uk-width-1-1" onchange="filterByOwner();">
+				<option value="all" selected="">
+					FILTER BY RECIPIENT
+				</option>
+				@foreach ($owners_array as $owner)
+				<option value="staff-{{$owner['id']}}"><a class="uk-dropdown-close">{{$owner['name']}}</a></option>
+				@endforeach
+			</select>
+		</div>
 
   	<div class="uk-width-1-1@s uk-width-1-5@m " style="vertical-align:top">
   		<a class="uk-button uk-button-success green-button uk-width-1-1" onclick="dynamicModalLoad('new-outbound-email-entry/{{ $project->id }}')">
@@ -40,31 +48,32 @@
   			<span>NEW MESSAGE</span>
   		</a>
   	</div>
-  </div>
+	</div>
+</div>
 
-  @if(count($messages))
-  <div uk-grid class="uk-margin-top uk-visible@m">
-  	<div class="uk-width-1-1">
-  		<div uk-grid>
-  			<div class=" uk-width-1-5@m uk-width-1-1@s">
-  				<div class="uk-margin-small-left"><small><strong>RECIPIENTS</strong></small></div>
-  			</div>
-  			<div class="uk-width-1-5@m uk-width-1-1@s">
-  				<div class="uk-margin-small-left"><small><strong>AUDIT</strong></small></div>
-  			</div>
-  			<div class="uk-width-2-5@m uk-width-1-1@s">
-  				<div class="uk-margin-small-left"><small><strong>SUMMARY</strong></small></div>
-  			</div>
-  			<div class="uk-width-1-5@m uk-width-1-1@s uk-text-right">
-  				<div class="uk-margin-right"><small><strong>DOCUMENTS</strong></small></div>
-  			</div>
-  		</div>
-  	</div>
-  </div>
-  @endif
+@if(count($messages))
+<div uk-grid class="uk-margin-top uk-visible@m">
+	<div class="uk-width-1-1">
+		<div uk-grid>
+			<div class=" uk-width-1-5@m uk-width-1-1@s">
+				<div class="uk-margin-small-left"><small><strong>RECIPIENTS</strong></small></div>
+			</div>
+			<div class="uk-width-1-5@m uk-width-1-1@s">
+				<div class="uk-margin-small-left"><small><strong>AUDIT</strong></small></div>
+			</div>
+			<div class="uk-width-2-5@m uk-width-1-1@s">
+				<div class="uk-margin-small-left"><small><strong>SUMMARY</strong></small></div>
+			</div>
+			<div class="uk-width-1-5@m uk-width-1-1@s uk-text-right">
+				<div class="uk-margin-right"><small><strong>DOCUMENTS</strong></small></div>
+			</div>
+		</div>
+	</div>
+</div>
+@endif
 
-  <div uk-grid class="uk-container uk-grid-collapse uk-margin-top uk-container-center" id="communication-list" style="width: 98%">
-  	@if(count($messages))
+<div uk-grid class="uk-container uk-grid-collapse uk-margin-top uk-container-center" id="communication-list" style="width: 98%">
+	@if(count($messages))
 	@foreach ($messages as $message)
 	<div class="filter_element uk-width-1-1 communication-list-item @if($message->owner)staff-{{ $message->owner->id }}@endif @if($message->project)program-{{ $message->project->id }}@endif  @if(count($message->local_documents) > 0 || count($message->docuware_documents) > 0) attachment-true @endif" uk-filter="outbound-phone" id="communication-{{ $message->id }}" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; top: 0px; left: 0px; opacity: 1;">
 		<div uk-grid class="communication-summary @if($message->unseen) communication-unread @endif">
@@ -137,101 +146,112 @@
 	</div>
 	@endforeach
 	@endif
-  </div>
-  <div id="list-tab-bottom-bar" class="uk-flex-middle"  style="height:50px;">
-  	<a  href="#top" uk-scroll="{offset: 90}" class="uk-button uk-button-default uk-button-small uk-align-right uk-margin-top uk-margin-right" style="margin-right:302px !important"><span class="a-arrow-small-up uk-text-small uk-vertical-align-middle"></span> SCROLL TO TOP</a>
+</div>
+<div id="list-tab-bottom-bar" class="uk-flex-middle"  style="height:50px;">
+	<a  href="#top" uk-scroll="{offset: 90}" class="uk-button uk-button-default uk-button-small uk-align-right uk-margin-top uk-margin-right" style="margin-right:302px !important"><span class="a-arrow-small-up uk-text-small uk-vertical-align-middle"></span> SCROLL TO TOP</a>
+</div>
 
-  </div>
-  <script>
+<script>
 
-  	function searchMessages(){
-  		$.post('{{ URL::route("communications.search") }}', {
-  			'communications-search' : $("#communications-search").val(),
-  			'_token' : '{{ csrf_token() }}'
-  		}, function(data) {
-  			if(data[0]!='1'){
-  				UIkit.modal.alert(data);
-  			} else {
-  				$('#project-detail-tab-2').trigger('click');
-  			}
-  		});
-  	}
+	function filterByOwner(){
+		var myGrid = UIkit.grid($('#communication-list'), {
+			controls: '#message-filters',
+			animation: false
+		});
+		var textinput = $("#filter-by-owner").val();
 
-  	function filterByOwner(){
-  		var myGrid = UIkit.grid($('#communication-list'), {
-  			controls: '#message-filters',
-  			animation: false
-  		});
-  		var textinput = $("#filter-by-owner").val();
+		@if(Auth::user()->isFromEntity(1))
+		$('#filter-by-program').prop('selectedIndex',0);
+		@endif
+		filterElement(textinput, '.filter_element');
+	}
 
-  		@if(Auth::user()->isFromEntity(1))
-  		$('#filter-by-program').prop('selectedIndex',0);
-  		@endif
-  		filterElement(textinput, '.filter_element');
-  	}
+	function filterElement(filterVal, filter_element){
+		if (filterVal === 'all') {
+			$(filter_element).show();
+		}
+		else {
+			$(filter_element).hide().filter('.' + filterVal).show();
+		}
+		UIkit.update(event = 'update');
+	}
 
-  	function filterElement(filterVal, filter_element){
-  		if (filterVal === 'all') {
-  			$(filter_element).show();
-  		}
-  		else {
-  			$(filter_element).hide().filter('.' + filterVal).show();
-  		}
-  		UIkit.update(event = 'update');
-  	}
+	function filterByProgram(){
+		var myGrid = UIkit.grid($('#communication-list'), {
+			controls: '#message-filters',
+			animation: false
+		});
+		var textinput = $("#filter-by-program").val();
+		$('#filter-by-owner').prop('selectedIndex',0);
+		filterElement(textinput, '.filter_element');
+	}
 
-
-  	function closeOpenMessage(){
-  		$('.communication-list-item').removeClass('communication-open');
-  		$('.communication-details').addClass('uk-hidden');
-  		$('.communication-summary').removeClass('uk-hidden');
-  	}
-
-  	function openMessage(communicationId){
-  		closeOpenMessage();
-  		$("#communication-"+communicationId).addClass('communication-open');
-  		$("#communication-"+communicationId+"-details").removeClass('uk-hidden');
-  		$("#communication-"+communicationId+"-summary").addClass('uk-hidden');
-  	}
-
-  	 // process search
-  	 $(document).ready(function() {
-  	 	$('.filter-attachments-button').click(function () {
-  	 		if( $(this).hasClass('uk-button-success') ){
-  	 			$(this).removeClass('uk-button-success');
-  	 			$('#filter-none').trigger('click');
-  	 		}else{
-  	 			$(this).addClass('uk-button-success');
-  	 			$('#filter-attachments').trigger('click');
-  	 		}
-  	 	});
+	function searchMessages(){
+		$.post('{{ URL::route("communications.search") }}', {
+			'communications-search' : $("#communications-project-search").val(),
+			'_token' : '{{ csrf_token() }}'
+		}, function(data) {
+			if(data[0]!='1'){
+				UIkit.modal.alert(data);
+			} else {
+				$('#project-detail-tab-2').trigger('click');
+			}
+		} );
+	}
 
 
-  	 	$('#communications-search').keydown(function (e) {
-  	 		if (e.keyCode == 13) {
-  	 			searchMessages();
-  	 			e.preventDefault();
-  	 			return false;
-  	 		}
-  	 	});
+	function closeOpenMessage(){
+		$('.communication-list-item').removeClass('communication-open');
+		$('.communication-details').addClass('uk-hidden');
+		$('.communication-summary').removeClass('uk-hidden');
+	}
 
-  	 	@if (session()->has('dynamicModalLoad') && session('dynamicModalLoad') != '' )
+	function openMessage(communicationId){
+		closeOpenMessage();
+		$("#communication-"+communicationId).addClass('communication-open');
+		$("#communication-"+communicationId+"-details").removeClass('uk-hidden');
+		$("#communication-"+communicationId+"-summary").addClass('uk-hidden');
+	}
 
-  	 	var dynamicModalLoadid = '';
-  	 	$.get( "/session/dynamicModalLoad", function( data ) {
-  	 		dynamicModalLoadid = data;
-  	 		console.log('Loading Message Id: '+dynamicModalLoadid);
 
-  	 		if(dynamicModalLoadid != ''){
-  	 			dynamicModalLoad("communication/0/replies/"+dynamicModalLoadid);
-  	 		}
-  	 	});
+	 // process search
+	 $(document).ready(function() {
+	 	$('.filter-attachments-button').click(function () {
+	 		if( $(this).hasClass('uk-button-success') ){
+	 			$(this).removeClass('uk-button-success');
+	 			$('#filter-none').trigger('click');
+	 		}else{
+	 			$(this).addClass('uk-button-success');
+	 			$('#filter-attachments').trigger('click');
+	 		}
+	 	});
 
-  	 	@endif
 
-  	 	var $filteredElements = $('.filter_element');
-  	 	$('.filter_link').click(function (e) {
-  	 		e.preventDefault();
+	 	$('#communications-project-search').keydown(function (e) {
+	 		if (e.keyCode == 13) {
+	 			searchMessages();
+	 			e.preventDefault();
+	 			return false;
+	 		}
+	 	});
+
+	 	@if (session()->has('dynamicModalLoad') && session('dynamicModalLoad') != '' )
+
+	 	var dynamicModalLoadid = '';
+	 	$.get( "/session/dynamicModalLoad", function( data ) {
+	 		dynamicModalLoadid = data;
+	 		console.log('Loading Message Id: '+dynamicModalLoadid);
+
+	 		if(dynamicModalLoadid != ''){
+	 			dynamicModalLoad("communication/0/replies/"+dynamicModalLoadid);
+	 		}
+	 	});
+
+	 	@endif
+
+	 	var $filteredElements = $('.filter_element');
+	 	$('.filter_link').click(function (e) {
+	 		e.preventDefault();
             // get the category from the attribute
             var filterVal = $(this).data('filter');
             filterElement(filterVal, '.filter_element');
@@ -243,5 +263,5 @@
             @endif
           });
 
-  	 });
-  	</script>
+	 });
+	</script>
