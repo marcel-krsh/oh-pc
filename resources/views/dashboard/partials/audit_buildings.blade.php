093152c1d75b76938b0726703bf33e08829cba5b
marcel-krsh <marcel.krsh@gmail.com>
2019-03-28 15:40:13 -0400

    oops

--- resources/views/dashboard/partials/audit_buildings.blade.php
+++ resources/views/dashboard/partials/audit_buildings.blade.php
@@ -284,6 +284,7 @@
 					@else
 					<div> <h2>!!! Missing data for this building is preventing it from being able to be viewed.</h2><a class="uk-button uk-button-small" onclick="$('#building-cache-{{$building->id}}).slideToggle();"> View record data:</a><div class="uk-width-1-1" id="building-cache-{{$building->id}}" style="display: none;">{{print_r($building)}}</div><p>Please click to view the record data and send a copy of this to your support team and they should be able to fix the missing data.</p>
 					</div>
+					@endIf
 					@endforeach
 				</div>
 			</div>
