7542c47f20bf2ea34289d946c92ea81c4989c648
marcel-krsh <marcel.krsh@gmail.com>
2019-03-29 11:50:11 -0400

    hot fix debug candidate: add building level amenity

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -195,7 +195,7 @@ class AuditController extends Controller
                 $amenity_inspection = AmenityInspection::where('audit_id', '=', $audit_id)->where('amenity_id', '=', $amenity_id)->whereNull('building_id')->whereNull('cachedbuilding_id')->first();
                 
                 if($amenity_inspection){
-                    $amenity_inspection->cachedbuilding_id = $building->building_id;
+                    $amenity_inspection->cachedbuilding_id = $building->building->id;
                     $amenity_inspection->save();
                 }
             }
