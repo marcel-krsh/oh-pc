07cd647feadadf9c6850002d32dff272b9162974
marcel-krsh <marcel.krsh@gmail.com>
2019-05-08 17:36:31 -0400

    correct spelling

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@
     Route::group(['middleware' => 'web'], function () {
         app('debugbar')->disable();
         
-        Route::get('/simple_compliance_test', 'SimplerComplianceSelection@runSimpleCompliance');
+        Route::get('/simple_compliance_test', 'SimpleComplianceSelection@runSimpleCompliance');
 
         
         // Route::get('/testProject/{project}', function($project){
