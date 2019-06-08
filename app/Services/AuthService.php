7d85f327a1181a107fc21b1d33d03bcd43839ed1
marcel-krsh <marcel.krsh@gmail.com>
2019-06-07 21:04:09 -0400

    Change to the auth service

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -225,7 +225,7 @@ class AuthService
      */
     public function userAuthenticateToken(int $user_id, string $user_token, $ip_address = null, $useragent = null)
     {
-        $endpoint = "{$this->_base_directory}/devco/user/authenticate_token?devcotoken={$user_token}&token={$this->_pcapi_access_token}&ipaddress={$ip_address}&useragent={$useragent}";
+        $endpoint = "{$this->_base_directory}/devco/user/nekot_etacitnehtua?devcotoken={$user_token}&token={$this->_pcapi_access_token}&ipaddress={$ip_address}&useragent={$useragent}";
 
         try {
             $response = $this->_client->request('GET', $endpoint);
