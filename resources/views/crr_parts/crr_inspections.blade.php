4806bd2ede35047938f4aad27ff53aaa3373a029
marcel-krsh <marcel.krsh@gmail.com>
2019-06-13 11:29:12 -0400

    Adding in counts

--- resources/views/crr_parts/crr_inspections.blade.php
+++ resources/views/crr_parts/crr_inspections.blade.php
@@ -3,27 +3,34 @@
 @if(isset($inspections_type) && $inspections_type == 'unit')
 <div uk-grid class="uk-margin-bottom">
 	<div class="uk-width-1-1 crr-blocks" style="page-break-inside: avoid;">
-		<h2>Units Inspected: </h2> @can('access_auditor') <small> <span class="use-hand-cursor" onclick="dynamicModalLoad('projects/{{$report->project->id}}/programs/0/summary',0,0,3);"><i class="a-arrow-diagonal-both use-hand-cursor" uk-tooltip="pos:top-left;title:CLICK TO SWAP UNITS;"  title="" aria-expanded="false"></i> SWAP UNITS </span> | </small>
+		<h2>Units Inspected: </h2> @can('access_auditor') <small> <span class="use-hand-cursor" onclick="dynamicModalLoad('projects/{{$report->project->id}}/programs/0/summary',0,0,3);"><i class="a-arrow-diagonal-both use-hand-cursor" uk-tooltip="pos:top-left;title:CLICK TO SWAP UNITS;"  title="" aria-expanded="false"></i> SWAP UNITS </span>  &nbsp;|  &nbsp;</small>
 			<style>
 				#modal-size {
 					height: 815px;
 				}
 			</style>
 		@endCan
-		<small><i class="a-folder"></i> : FILE INSPECTION &nbsp;|  &nbsp;<i class="a-mobile"></i> : SITE INSPECTION </small>
-		<hr class="dashed-hr uk-margin-bottom">
-		<?php //dd($i);
-		$siteVisited = array();
-		$fileVisited = array();
-		$nameOutput = array();
-		$loops = 0;
-		if(is_array($inspections) && count($inspections)> 0){
-			$currentUnit = 0;
-		}
-		$inspections = collect($inspections);
-		$inspections =$inspections->sortBy('unit_name');
-		//dd($inspections);
+
+		<?php 
+
+			$siteVisited = array();
+			$fileVisited = array();
+			$nameOutput = array();
+			$loops = 0;
+			if(is_array($inspections) && count($inspections)> 0){
+				$currentUnit = 0;
+			}
+			$inspections = collect($inspections);
+			$inspections =$inspections->sortBy('unit_name');
+			//dd($inspections);
+		
+			$fileInspections = count(collect($inspections)->where('is_site_visit',0));
+			$siteInspections = count(collect($inspections)->where('is_site_visit',1));
+
 		?>
+		<small><i class="a-folder"></i> : @if($fileInspections > 1 || $fileInspections < 1) {{$fileInspections}} FILE INSPECTIONS @else {{$fileInspections}} FILE INSPECTION @endIf &nbsp;|  &nbsp;<i class="a-mobile"></i> : @if($siteInspections > 1 || $siteInspections < 1) {{$siteInspections}} SITE INSPECTIONS @else {{$siteInspections}} SITE INSPECTION @endIf </small>
+		<hr class="dashed-hr uk-margin-bottom">
+		
 		<div class="uk-column-1-3 uk-column-divider">
 			@forEach($inspections as $i)
 			<?php $noShow = 0 ; ?>
@@ -43,17 +50,17 @@
 					@forEach($thisUnitValues as $g)
 					<?php //dd($thisUnitValues, $g); ?>
 					@if($g->is_site_visit == 1)
-					@if(!in_array($g->unit_id, $siteVisited))
-					<i class="a-mobile uk-text-large uk-margin-small-right @can('access_auditor') @if(!$print)use-hand-cursor @endif @endcan" @can('access_auditor') @if(!$print) onclick="openFindings(this, {{ $report->audit->id }}, null, {{ $g->unit_id }}, null, null,'0');" @endif @endcan></i> <?php $siteVisited[] =$g->unit_id;  ?>
-					@else
-					<?php $noShow = 1; ?>
-					@endIf
+						@if(!in_array($g->unit_id, $siteVisited))
+						<i class="a-mobile uk-text-large uk-margin-small-right @can('access_auditor') @if(!$print)use-hand-cursor @endif @endcan" @can('access_auditor') @if(!$print) onclick="openFindings(this, {{ $report->audit->id }}, null, {{ $g->unit_id }}, null, null,'0');" @endif @endcan></i> <?php $siteVisited[] =$g->unit_id;  ?>
+						@else
+						<?php $noShow = 1; ?>
+						@endIf
 
 					@elseIf(!in_array($g->unit_id, $fileVisited))
-					@if(!in_array($g->unit_id, $siteVisited))
-					<i class="a-mobile uk-text-large uk-margin-small-right @can('access_auditor')@if(!$print)use-hand-cursor @endif @endcan" style="color:rgba(0,0,0,0);" @can('access_auditor')@if(!$print) onclick="openFindings(this, {{ $report->audit->id }}, null, {{ $g->unit_id }}, null, null,'0');" @endif  @endcan></i>
-					@endIf
-					<i class="a-folder uk-text-large @can('access_auditor')@if(!$print)use-hand-cursor @endif @endcan" @can('access_auditor')@if(!$print) onclick="openFindings(this, {{ $report->audit->id }}, null, {{ $g->unit_id }}, 'file',null,'0');" @endif @endcan></i> <?php $fileVisited[]=$g->unit_id; ?>
+						@if(!in_array($g->unit_id, $siteVisited))
+						<i class="a-mobile uk-text-large uk-margin-small-right @can('access_auditor')@if(!$print)use-hand-cursor @endif @endcan" style="color:rgba(0,0,0,0);" @can('access_auditor')@if(!$print) onclick="openFindings(this, {{ $report->audit->id }}, null, {{ $g->unit_id }}, null, null,'0');" @endif  @endcan></i>
+						@endIf
+						<i class="a-folder uk-text-large @can('access_auditor')@if(!$print)use-hand-cursor @endif @endcan" @can('access_auditor')@if(!$print) onclick="openFindings(this, {{ $report->audit->id }}, null, {{ $g->unit_id }}, 'file',null,'0');" @endif @endcan></i> <?php $fileVisited[]=$g->unit_id; ?>
 
 					@else
 					<?php $noShow = 1; ?>
