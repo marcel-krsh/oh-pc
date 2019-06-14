1a1df4963ee617302a22ea6780b56924be10fd76
marcel-krsh <marcel.krsh@gmail.com>
2019-06-13 21:34:58 -0400

    Data is the current version

--- resources/views/crr_parts/crr_inspections.blade.php
+++ resources/views/crr_parts/crr_inspections.blade.php
@@ -82,7 +82,7 @@
 				@if(!in_array($i->unit_id, $nameOutput))
 				<div  style="float: left;"  >
 					<?php 
-						dd($report);
+						dd($data);
 					?>
 					@if($print !== 1)<a href="#findings-list" class="uk-link-mute" onClick="showOnlyFindingsFor('unit-{{$i->unit_id}}-finding');">
 
