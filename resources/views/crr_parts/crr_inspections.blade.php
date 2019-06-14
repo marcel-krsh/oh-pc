71fe789f3f9ccedaf5b5f7c8ca98e6ebab56e4ae
marcel-krsh <marcel.krsh@gmail.com>
2019-06-14 01:46:13 -0400

    missing space

--- resources/views/crr_parts/crr_inspections.blade.php
+++ resources/views/crr_parts/crr_inspections.blade.php
@@ -175,7 +175,7 @@
 
 					@elseIf(!in_array($g->unit_id, $fileVisited))
 						@if(!in_array($g->unit_id, $siteVisited))
-						<span style="opacity:.3;"><i class="a-mobile uk-text-large uk-margin-small-right "  ></i><i class="a-circle-minus on-phone"></i></span>
+						<span style="opacity:.3;"><i class="a-mobile uk-text-large uk-margin-small-right "  ></i> <i class="a-circle-minus on-phone"></i></span>
 						@endIf
 						<i class="a-folder uk-text-large @can('access_auditor')@if(!$print)use-hand-cursor @endif @endcan" @can('access_auditor')@if(!$print) onclick="openFindings(this, {{ $report->audit->id }}, null, {{ $g->unit_id }}, 'file',null,'0');" @endif @endcan></i> @if($thisUnitFileFindings > 0) <span class="uk-badge finding-number on-folder">{{$thisUnitFileFindings}}</span> @else<i class="a-circle-checked on-folder no-findings"></i>@endIf<?php $fileVisited[]=$g->unit_id; ?>
 
