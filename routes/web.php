2e5b1a2417cac6f88c13e67ec82ae13e38feb0d5
marcel-krsh <marcel.krsh@gmail.com>
2019-07-25 04:39:13 +0000

    Project contact users, orgs, address: edits, defaults for reports selection

--- routes/web.php
+++ routes/web.php
@@ -244,6 +244,22 @@
             Route::post('/modals/{project}/add-user-to-project', 'ProjectContactsController@saveAddUserToProject')->name('project.add-user');
             Route::get('/modals/remove-user-from-project/{project}/{user}', 'ProjectContactsController@removeUserFromProject');
             Route::post('/modals/{project}/remove-user-from-project', 'ProjectContactsController@deleteAddUserToProject')->name('project.remove-user');
+            Route::get('/modals/add-organization-to-user/{user}/{project}', 'ProjectContactsController@addOrganizationToUser');
+            Route::post('/modals/{user}/add-organization-to-user', 'ProjectContactsController@saveOrganizationToUser')->name('user.add-organization-to-user');
+            Route::get('/modals/edit-organization-of-user/{org}/{project}', 'ProjectContactsController@editOrganizationOfUser');
+            Route::post('/modals/{org}/edit-organization-of-user', 'ProjectContactsController@saveOrganizationOfUser')->name('user.edit-organization-of-user');
+            Route::post('/modals/{org}/remove-organization-of-user', 'ProjectContactsController@removeOrganizationOfUser')->name('user.remove-organization-of-user');
+            Route::post('/modals/make-project-default-organization', 'ProjectContactsController@defaultOrganizationOfUserForProject')->name('user.make-project-default-organization');
+            Route::get('/modals/edit-name-of-user/{user}/{project}', 'ProjectContactsController@editNameOfUser');
+            Route::post('/modals/{user}/edit-name-of-user', 'ProjectContactsController@saveNameOfUser')->name('user.edit-name-of-user');
+            Route::post('/modals/make-project-default-user', 'ProjectContactsController@defaultUserForProject')->name('user.make-project-default-user');
+            Route::get('/modals/add-address-to-user/{user}/{project}', 'ProjectContactsController@addAddressToUser');
+            Route::post('/modals/{user}/add-address-to-user', 'ProjectContactsController@saveAddressToUser')->name('user.add-address-to-user');
+            Route::post('/modals/make-project-default-address', 'ProjectContactsController@defaultAddressOfUserForProject')->name('user.make-project-default-address');
+            Route::get('/modals/edit-address-of-user/{address}/{project}', 'ProjectContactsController@editAddressOfUser');
+            Route::post('/modals/{address}/edit-address-of-user', 'ProjectContactsController@saveAddressOfUser')->name('user.edit-address-of-user');
+            Route::post('/modals/{address}/remove-address-of-user', 'ProjectContactsController@removeAddressOfUser')->name('user.remove-address-of-user');
+
 
 
 
