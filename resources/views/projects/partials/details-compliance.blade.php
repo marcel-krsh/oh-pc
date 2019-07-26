09c3a68022079575abca9fc02b550216f2b21762
marcel-krsh <marcel.krsh@gmail.com>
2019-07-25 21:07:12 -0400

    Another change

--- resources/views/projects/partials/details-compliance.blade.php
+++ resources/views/projects/partials/details-compliance.blade.php
@@ -11,7 +11,7 @@
 				<div>
 					<canvas id="chartjs-summary" class="chartjs" style="display: block;"></canvas>
 				</div>
-				<a onclick="dynamicModalLoad('projects/{{$data["project"]["id"]}}/programs/0/summary',0,0,1);" class="uk-button uk-link proj_details proj_details_1" style="margin-top: 18px;"><i class="a-arrow-diagonal-both use-hand-cursor" ></i> SWAP UNITS</a>
+				<a onclick="dynamicModalLoad('projects/{{$data["project"]["id"]}}/programs/0/summary',0,0,1);" class="uk-button uk-button-small uk-button-success " style="margin-top: 18px;"><i class="a-arrow-diagonal-both use-hand-cursor" ></i> SWAP UNITS</a>
 			</div>
 			<div class="uk-width-2-5">
 				<table class="uk-table uk-table-small noline small-padding">
