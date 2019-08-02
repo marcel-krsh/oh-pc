51733d9cf6f82f950e74e4951b56faa67ebc1690
marcel-krsh <marcel.krsh@gmail.com>
2019-08-01 23:03:13 -0400

    use the onchange

--- resources/views/crr_parts/crr_findings_groups.blade.php
+++ resources/views/crr_parts/crr_findings_groups.blade.php
@@ -93,7 +93,7 @@
 							@endif
 						</div>
 						<div  class="uk-width-1-2 uk-margin-remove">
-							<input id="resolved-date-finding-{{$f->id}}" class="uk-input flatpickr flatpickr-input" style="width:100%;" readonly type="text" placeholder="DATE" value="">
+							<input id="resolved-date-finding-{{$f->id}}" class="uk-input flatpickr flatpickr-input" style="width:100%;" readonly type="text" placeholder="DATE" value="" onchange="resolveFinding({{ $f->id }},$(this).val());">
 							
 
 						  <script>
