6c8d406b28a14fc32615f15ee877532c61e64060
marcel-krsh <marcel.krsh@gmail.com>
2019-08-01 22:39:21 -0400

    Remove extra bracket

--- resources/views/crr_parts/crr_findings_groups.blade.php
+++ resources/views/crr_parts/crr_findings_groups.blade.php
@@ -104,12 +104,14 @@
 									// "locale": {
 							  		//"firstDayOfWeek": 1 // start week on Monday
 							      },
-							      
+							      onOpen: function(dateStr){
+							      	console.log('Calendar opened for finding {{$f->id}}'),
+							      },
 							      onClose: function(selectedDates, dateStr, instance){
 							      	console.log('Calling resolve finding {{$f->id}} dateStr'),
 							      	resolveFinding({{ $f->id }},dateStr)
 							      	
-							      }
+							      
 							    });
 
 						  </script>
