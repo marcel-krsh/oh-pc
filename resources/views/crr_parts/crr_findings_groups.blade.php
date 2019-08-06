3caedc2cbcff3e580ee92450b6533236a4b30217
marcel-krsh <marcel.krsh@gmail.com>
2019-08-05 21:27:13 -0400

    for all headers

--- resources/views/crr_parts/crr_findings_groups.blade.php
+++ resources/views/crr_parts/crr_findings_groups.blade.php
@@ -23,7 +23,7 @@
 						<small style="text-transform: uppercase;">{{$f->building->address->line_1}} {{$f->building->address->line_2}} | 
 						{{$f->building->address->city}}, {{$f->building->address->state}} {{$f->building->address->zip}}</small>
 					@endIf
-					<hr class="dashed-hr uk-margin-bottom">
+					@if($print)<hr class="dashed-hr uk-margin-bottom"> @endif
 				</div> 
 			@endif
 			
@@ -31,7 +31,7 @@
 		@elseIf(!is_null($f->unit_id))
 			@if ($findingHeader !== $f->unit->unit_name)
 				@php $findingHeader = $f->unit->unit_name; $columnCount = 1; $findingsRun = 1; @endphp
-				<div class="uk-width-1-1 uk-margin-bottom @if($f->unit_id > 0) unit-{{$f->unit_id}}-finding @endIf @if($f->building_id > 0) building-{{$f->building_id}}-finding @endIf @if(null == $f->unit_id && null == $f->building_id) site-amenity-finding-{{$f->id}} @endIf @if(isset($site_finding) && $site_finding == 1) site-{{ $f->amenity->amenity_type_key }}-finding @endif finding-group uk-contrast"  style="background: #4e4e4e; padding-top: 7px; @if($oneColumn) margin-bottom: 0px !important; @endIf">
+				<div class="uk-width-1-1 uk-margin-bottom @if($f->unit_id > 0) unit-{{$f->unit_id}}-finding @endIf @if($f->building_id > 0) building-{{$f->building_id}}-finding @endIf @if(null == $f->unit_id && null == $f->building_id) site-amenity-finding-{{$f->id}} @endIf @if(isset($site_finding) && $site_finding == 1) site-{{ $f->amenity->amenity_type_key }}-finding @endif finding-group uk-contrast"  style="background: #4e4e4e; padding-top: 11px; @if($oneColumn) margin-bottom: 0px !important; @endIf">
 					<h3 class="uk-margin-remove">UNIT FINDINGS FOR UNIT: {{$f->unit->unit_name}}</h3> @if(!is_null($f->unit->building_id))IN BIN: {{$f->unit->building->building_name}} <br />
 						@if(!is_null($f->unit->building->address))
 							<small style="text-transform: uppercase;">{{$f->unit->building->address->line_1}} {{$f->unit->building->address->line_2}} | 
@@ -45,13 +45,13 @@
 		@else
 			@if ($findingHeader !== $f->project->project_name)
 				@php $findingHeader = $f->project->project_name; $columnCount = 1; $findingsRun = 1; @endphp
-				<div class="uk-width-1-1 uk-margin-bottom @if($f->unit_id > 0) unit-{{$f->unit_id}}-finding @endIf @if($f->building_id > 0) building-{{$f->building_id}}-finding @endIf @if(null == $f->unit_id && null == $f->building_id) site-amenity-finding-{{$f->id}} @endIf @if(isset($site_finding) && $site_finding == 1) site-{{ $f->amenity->amenity_type_key }}-finding @endif finding-group uk-contrast"  style="background: #4e4e4e; padding-top: 7px; @if($oneColumn) margin-bottom: 0px !important; @endIf">
+				<div class="uk-width-1-1 uk-margin-bottom @if($f->unit_id > 0) unit-{{$f->unit_id}}-finding @endIf @if($f->building_id > 0) building-{{$f->building_id}}-finding @endIf @if(null == $f->unit_id && null == $f->building_id) site-amenity-finding-{{$f->id}} @endIf @if(isset($site_finding) && $site_finding == 1) site-{{ $f->amenity->amenity_type_key }}-finding @endif finding-group uk-contrast"  style="background: #4e4e4e; padding-top: 11px; @if($oneColumn) margin-bottom: 0px !important; @endIf">
 					<h3 class="uk-margin-remove">SITE FINDINGS FOR: {{$f->project->project_name}}</h3>
 					@if($f->project->address)
 						<small style="text-transform: uppercase;"> {{$f->project->address->line_1}} {{$f->project->address->line_2}} | 
 						{{$f->project->address->city}}, {{$f->project->address->state}} {{$f->project->address->zip}} </small>
 					@endIf
-					<hr class="dashed-hr uk-margin-bottom">
+					@if($print)<hr class="dashed-hr uk-margin-bottom"> @endif
 				</div> 
 			@endif
 			
