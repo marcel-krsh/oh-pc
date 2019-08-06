03f9b2ebcb6535c5662c18b5077f07dc294d2622
marcel-krsh <marcel.krsh@gmail.com>
2019-08-06 14:34:52 -0400

    script changes

--- resources/views/crr_parts/crr_findings.blade.php
+++ resources/views/crr_parts/crr_findings.blade.php
@@ -199,7 +199,7 @@ foreach($grouped_bf as $bk => $bf) {
 		}, function(data) {
 			if(data != 0){
 				console.log('Resolution saved for finding '+resolveFindingId);
-				$('#inspec-tools-finding-resolve-'+resolveFindingId).html('<button class="uk-button uk-link uk-margin-small-left uk-width-1-2" uk-tooltip="pos:top-left;title:RESOLVED ON '+data.toUpperCase()+';" onclick="resolveFinding('+resolveFindingId+',\'null\')"><span class="a-circle-cross">&nbsp;</span>CLEAR</button>');
+				$('#inspec-tools-finding-resolve-'+resolveFindingId).html('<button class="uk-button uk-link uk-margin-small-left uk-width-1-2" onclick="resolveFinding(\''+resolveFindingId+'\',\'null\')"><span class="a-circle-cross">&nbsp;</span>CLEAR</button>');
 			}else{
 				console.log('Resolution cleared for finding '+resolveFindingId);
 				$('#inspec-tools-finding-resolve-'+resolveFindingId).html('RESOLVED AT:');
