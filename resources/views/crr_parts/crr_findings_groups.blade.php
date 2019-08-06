ac766727f4365c9b4ef1359072e626cea163d695
marcel-krsh <marcel.krsh@gmail.com>
2019-08-05 21:20:43 -0400

    More compact

--- resources/views/crr_parts/crr_findings_groups.blade.php
+++ resources/views/crr_parts/crr_findings_groups.blade.php
@@ -18,10 +18,10 @@
 
 				@php $findingHeader = $f->building->building_name; $columnCount = 1; $findingsRun = 1; @endphp
 				<div class="uk-width-1-1 uk-margin-bottom @if($f->unit_id > 0) unit-{{$f->unit_id}}-finding @endIf @if($f->building_id > 0) building-{{$f->building_id}}-finding @endIf @if(null == $f->unit_id && null == $f->building_id) site-amenity-finding-{{$f->id}} @endIf @if(isset($site_finding) && $site_finding == 1) site-{{ $f->amenity->amenity_type_key }}-finding @endif finding-group uk-contrast" style="background: #4e4e4e; padding-top: 7px; @if($oneColumn) margin-bottom: 0px !important; @endIf">
-					<h3 class="uk-margin-remove">FINDINGS FOR BIN: {{$f->building->building_name}}</h3>
+					<h3 class="uk-margin-remove">BUILDING FINDINGS FOR BIN: {{$f->building->building_name}}</h3>
 					@if(!is_null($f->building->address))
-						{{$f->building->address->line_1}} {{$f->building->address->line_2}}<br />
-						{{$f->building->address->city}}, {{$f->building->address->state}} {{$f->building->address->zip}}
+						<small style="text-transform: uppercase;">{{$f->building->address->line_1}} {{$f->building->address->line_2}} | 
+						{{$f->building->address->city}}, {{$f->building->address->state}} {{$f->building->address->zip}}</small>
 					@endIf
 					<hr class="dashed-hr uk-margin-bottom">
 				</div> 
@@ -32,10 +32,10 @@
 			@if ($findingHeader !== $f->unit->unit_name)
 				@php $findingHeader = $f->unit->unit_name; $columnCount = 1; $findingsRun = 1; @endphp
 				<div class="uk-width-1-1 uk-margin-bottom @if($f->unit_id > 0) unit-{{$f->unit_id}}-finding @endIf @if($f->building_id > 0) building-{{$f->building_id}}-finding @endIf @if(null == $f->unit_id && null == $f->building_id) site-amenity-finding-{{$f->id}} @endIf @if(isset($site_finding) && $site_finding == 1) site-{{ $f->amenity->amenity_type_key }}-finding @endif finding-group uk-contrast"  style="background: #4e4e4e; padding-top: 7px; @if($oneColumn) margin-bottom: 0px !important; @endIf">
-					<h3 class="uk-margin-remove">FINDINGS FOR UNIT: {{$f->unit->unit_name}}</h3> @if(!is_null($f->unit->building_id))IN BIN: {{$f->unit->building->building_name}} <br />
+					<h3 class="uk-margin-remove">UNIT FINDINGS FOR UNIT: {{$f->unit->unit_name}}</h3> @if(!is_null($f->unit->building_id))IN BIN: {{$f->unit->building->building_name}} <br />
 						@if(!is_null($f->unit->building->address))
-							{{$f->unit->building->address->line_1}} {{$f->unit->building->address->line_2}}<br />
-							{{$f->unit->building->address->city}}, {{$f->unit->building->address->state}} {{$f->unit->building->address->zip}}
+							<small style="text-transform: uppercase;">{{$f->unit->building->address->line_1}} {{$f->unit->building->address->line_2}} | 
+							{{$f->unit->building->address->city}}, {{$f->unit->building->address->state}} {{$f->unit->building->address->zip}}</small>
 						@endIf
 					@endIf
 					<hr class="dashed-hr uk-margin-bottom">
@@ -48,8 +48,8 @@
 				<div class="uk-width-1-1 uk-margin-bottom @if($f->unit_id > 0) unit-{{$f->unit_id}}-finding @endIf @if($f->building_id > 0) building-{{$f->building_id}}-finding @endIf @if(null == $f->unit_id && null == $f->building_id) site-amenity-finding-{{$f->id}} @endIf @if(isset($site_finding) && $site_finding == 1) site-{{ $f->amenity->amenity_type_key }}-finding @endif finding-group uk-contrast"  style="background: #4e4e4e; padding-top: 7px; @if($oneColumn) margin-bottom: 0px !important; @endIf">
 					<h3 class="uk-margin-remove">SITE FINDINGS FOR: {{$f->project->project_name}}</h3>
 					@if($f->project->address)
-						{{$f->project->address->line_1}} {{$f->project->address->line_2}}<br />
-						{{$f->project->address->city}}, {{$f->project->address->state}} {{$f->project->address->zip}}
+						<small style="text-transform: uppercase;"> {{$f->project->address->line_1}} {{$f->project->address->line_2}} | 
+						{{$f->project->address->city}}, {{$f->project->address->state}} {{$f->project->address->zip}} </small>
 					@endIf
 					<hr class="dashed-hr uk-margin-bottom">
 				</div> 
