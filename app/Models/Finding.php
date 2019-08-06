95ef07e10f60e4444932536c1eb5520d8a5c1d01
marcel-krsh <marcel.krsh@gmail.com>
2019-08-06 15:13:40 -0400

    building

--- app/Models/Finding.php
+++ app/Models/Finding.php
@@ -44,7 +44,7 @@ class Finding extends Model
     {
         return $this->hasOne(\App\Models\Unit::class, 'id', 'unit_id');
     }
-    public function findingBuilding(): HasOne
+    public function building(): HasOne
     {
         return $this->hasOne(\App\Models\Building::class, 'id', 'building_id');
     }
