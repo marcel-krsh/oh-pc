b290cf2c77ee95689e9932eee563cb302f08c25f
marcel-krsh <marcel.krsh@gmail.com>
2019-09-22 11:55:26 -0400

    correct cached audits

--- app/Jobs/update_audit_caches.php
+++ app/Jobs/update_audit_caches.php
@@ -30,7 +30,8 @@ class update_audit_caches implements ShouldQueue
      */
     public function handle()
     {
-        $audits = CachedAudit::where('step_id'< 67){
+        $audits = CachedAudit::where('step_id'< 67)->get();
+
             // processing only active audits.
             foreach ($audits as $audit) {
                $audit->update_cached_audit();
