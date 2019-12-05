d0abb0be66b27bf562658c4e06548923382850b8
marcel-krsh <marcel.krsh@gmail.com>
2019-12-05 03:41:57 +0000

    added script to send notifications for the communications generated between Dec 1 and now

---
+++ app/Http/Controllers/AdminfixController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\CommunicationRecipient;
+use Carbon\Carbon;
+use Event;
+
+class AdminfixController extends Controller
+{
+  /**
+   * Created to fix the non-triggered notifications from 12-01-2019 to NOW. Recommended to run once
+   * @return [type] [description]
+   */
+  public function communicationNotifications()
+  {
+    if (!session()->has('ran-notification')) {
+      //get all the messages sent to receipients with communication and user, which are not seen
+      $receipients = CommunicationRecipient::with('communication', 'user')->whereBetween('created_at', ['2019-12-01 00:00:00', Carbon::now()])->where('seen', '<>', 1)->get();
+      foreach ($receipients as $key => $receipient) {
+        //Check if the notification was already triggered for this message - HOW!!!
+        Event::dispatch('communication.created', $receipient);
+        // event(new CommunicationReceipientEvent($receipient->id));
+      }
+      session(['ran-notification' => 1]);
+      return 'Processed, DONOT RUN AGAIN';
+    } else {
+      return 'Already ran';
+    }
+  }
+}
