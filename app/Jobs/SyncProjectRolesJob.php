c95e72f9f7559ef4059004b89a2503484f797d56
marcel-krsh <marcel.krsh@gmail.com>
2019-12-19 15:28:14 -0500

    Fix logic of sync so it does not skip records.

--- app/Jobs/SyncProjectRolesJob.php
+++ app/Jobs/SyncProjectRolesJob.php
@@ -96,7 +96,8 @@ class SyncProjectRolesJob implements ShouldQueue
 
                             // convert dates to seconds and miliseconds to see if the current record is newer.
                             $devcoDate = new DateTime($v['attributes']['lastEdited']);
-                            $allitaDate = new DateTime($lastModifiedDate->last_edited_convert);
+                            $allitaDate = new DateTime($updateRecord->last_edited);
+                            
                             $allitaFloat = ".".$allitaDate->format('u');
                             $devcoFloat = ".".$devcoDate->format('u');
                             settype($allitaFloat, 'float');
