48fd3b2070ccce51db757410578b2a54567837e9
marcel-krsh <marcel.krsh@gmail.com>
2020-03-21 23:16:03 +0000

    controller fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -26,9 +26,10 @@ class Controller extends BaseController
 		//dd('hello world');
 		$this->middleware('allita.auth');
 		$this->middleware(function ($request, $next) {
-			$this->user = Auth::user()->load('roles');
+			$this->user = Auth::user();
 			//dd('My life as a user',$this->user);
 			if ($this->user != null) {
+				$this->user = Auth::user()->load('roles');
 				$this->auditor_access = $this->access($this->user, 'auditor');
 				// $this->auditor_access = $this->user->auditor_access();
 				View::share('auditor_access', $this->auditor_access);
