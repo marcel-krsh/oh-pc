15b7651c1be1dc69232da250c0ffe6e156980896
marcel-krsh <marcel.krsh@gmail.com>
2020-03-24 22:37:28 -0400

    Make the PM views follow the rules and use updated search logic

--- resources/views/projects/partials/pm-local-documents.blade.php
+++ resources/views/projects/partials/pm-local-documents.blade.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css{{ asset_version() }}">
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
 <style>
 	/*for toggler*/
 	#allita-documents .switch {
@@ -166,7 +169,7 @@
 		<hr>
 		<div uk-grid class="uk-grid-collapse">
 			<div class="uk-width-1-1" uk-grid>
-				<input type="text" id="local-documents-search" class="filter-box uk-width-1-5" placeholder="SEARCH DOCUMENTS">
+				<input type="text" value="{{ session()->has('local-documents-search-term') ? $searchTerm : '' }}" id="local-documents-search" class="filter-box uk-width-1-5" placeholder="SEARCH DOCUMENTS">
 				<div class="uk-width-1-5">
 					<label class="switch">
 						<input type="checkbox" onchange="searchDocuments(this);" id="documents-unreviewed-checkbox" @if($unreviewed) checked="true" @endIf >
@@ -191,22 +194,17 @@
 						<span class="slider round"></span>
 					</label> <small>ALL  FINDINGS RESOLVED</small>
 				</div>
-
-
 				@if($searchTerm != null)
 				<div class="uk-width-1-1 uk-margin-remove-top">
 					<div  class="uk-badge uk-text-right badge-filter">
-
 						<a onclick="$('#local-documents-search').val(''); searchDocuments();" class="uk-dark uk-light use-hand-cursor" style="position: relative;top: -3px; margin-right: 9px;"><i class="a-circle-cross"></i> <span> {{ strtoupper($searchTerm) }}</span></a>
-
 					</div>
 					<div class="uk-badge uk-text-right badge-filter" style="    background: #6aa26a;">
-						<a uk-tooltip title="EXPLAIN RESULTS" onclick="UIkit.modal.alert('<h1>What Did My Search... Search?</h1><h3>The Query Searches: <ul><li>Document Categories</li><li>Finding Types</li><li>Building Names</li><li>Unit Names</li><li>Amenity Names</li><li>First and Last Names of Uploaders</li><li>Audit Numbers</li><li>Finding Number</li></h3>');" class="uk-dark uk-light use-hand-cursor" style="position: relative; top: -2px;margin-right: 1.5px;padding-left: 0px;">?
+						<a uk-tooltip title="EXPLAIN RESULTS" onclick="UIkit.modal.alert('<h1>What Did My Search... Search?</h1><h3>The Query Searches: <ul><li>Document Categories</li><li>Finding Types</li><li>Building Names</li><li>Unit Names</li><li>Amenity Names</li><li>First and Last Names of Uploaders</li><li>Audit Numbers</li><li>Finding Number</li></h3>');" class="uk-dark uk-light use-hand-cursor" style="position: relative;top: -2px;margin-right: 1.5px;padding-left: 0px;">?
 						</a>
 					</div>
 				</div>
 				@endif
-
 			</div>
 			<hr class="uk-width-1-1 uk-margin-top">
 			<div class="uk-width-1-1 uk-margin-top" id="documents-tab-pages-and-filters" >
@@ -234,125 +232,136 @@
 			<tbody id="sent-document-list" style="font-size: 13px">
 				@foreach ($documents as $document)
 				@php
-				$document_findings = $findings->whereIn('id', $document->finding_ids);
-				// dd($document_findings);
+				$document_findings = $document->all_findings();
+				$buildings = !is_null($document->building_ids) ? ($document->building_ids) : [];
+				$units = !is_null($document->unit_ids) ? ($document->unit_ids) : [];
+				// if(!is_array($units))
+				// dd($document);
 				$audits_ids = ($document->audits->pluck('id')->toArray());
 				$document_finding_audit_ids = $document_findings->pluck('audit_id')->toArray();
 				$all_ids = array_merge($audits_ids, $document_finding_audit_ids, [$document->audit_id]);
-				$document_audits = $audits->whereIn('id', $all_ids);
+				$all_ids = collect($all_ids)->unique()->filter()->toArray();
+				// $document_audits = $audits->whereIn('id', $all_ids);
+
 				$site_findings = $document_findings->where('building_id', null)->where('unit_id', null);
 				$building_findings = $document_findings->where('building_id', '<>', null)->where('unit_id', null);
 				$unit_findings = $document_findings->where('building_id', null)->where('unit_id', '<>', null);
+
 				$resolved_findings = count(collect($document_findings)->where('auditor_approved_resolution', 1));
 				$unresolved_findings = count($document_findings) - $resolved_findings;
+
 				// dd(count($site_findings));
 				// $x = $unit_findings->where('unit.building_id', 24961);
 				// $thisUnitFileFindings = count(collect($findings)->where('unit_id', $i->unit_id)->where('finding_type.type', 'file'));
 				// $thisUnitSiteFindings = count(collect($findings)->where('unit_id', $i->unit_id)->where('finding_type.type', '!=', 'file'));
 				// dd($document_audits);
 				@endphp
-				<tr class="all @foreach ($document_audits as $audit)audit-{{ $audit->id }} @endforeach @if($document->has_findings) @foreach($document_findings as $finding)finding-{{ $finding->id }} @endforeach @endif @foreach($document->assigned_categories as $category)category-{{ $category->id }} @endforeach @if($document->unit_id != NULL) unit-{{ $document->unit_id }} @endIf">
+				<tr class="all @foreach ($document->audits as $audit)audit-{{ $audit->id }} @endforeach
+
+
+					@foreach($document_findings as $finding)finding-{{ $finding->id }}
+
+
+
+					 @endforeach @foreach($document->assigned_categories as $category)category-{{ $category->id }} @endforeach
+
+
+					 @foreach($units as $unit)finding-{{ $unit }} @endforeach
+
+
+					 @foreach($buildings as $building)finding-{{ $building }} @endforeach">
 					<td style="vertical-align: middle;"><span class="uk-margin-top uk-padding-left" uk-tooltip title="{{ $document->created_at->format('h:i a') }}">{{ date('m/d/Y', strtotime($document->created_at)) }}</span>
 					</td>
 					<td class="uk-width-1-2" style="vertical-align: middle;">
 						<ul class="uk-list document-category-menu">
 							@foreach ($document->assigned_categories as $document_category)
-		    				{{-- @if(!is_null($document->ohfa_file_path))
-		    				<li>
-		    					<a>{{ $document_category->document_category_name }} : {{ ucwords(strtolower($document->filename)) }}</a>
-		    				</li>
-		    				@else --}}
-		    				<div class="uk-padding-remove" style="margin-top: 7px;">
-		    					<span id="audit-avatar-badge-1" uk-tooltip="pos:top-left;title:{{ $document->user->full_name() }};" title="" aria-expanded="false" class="user-badge user-badge-{{ $document->user->badge_color }}"> {{ $document->user->initials() }}
-		    					</span>
-		    				</div>
+							<div class="uk-padding-remove" style="margin-top: 7px;">
+								<span id="audit-avatar-badge-1" uk-tooltip="pos:top-left;title:{{ $document->user->full_name() }};" title="" aria-expanded="false" class="user-badge user-badge-{{ $document->user->badge_color }}"> {{ $document->user->initials() }}
+								</span>
+							</div>
+							<li class="{{ ($document->notapproved == 1) ? "declined-category s" : "" }} {{ ($document->approved == 1) ? "approved-category" : "" }}">
+								<a id="sent-id-{{ $document->id }}-category-id-{{ $document_category->id }}" class="">
+									<span style="float: left;margin-top:8px;margin-left: 5px"  id="sent-id-{{ $document->id }}-category-id-1-recieved-icon" class="a-checkbox-checked {{ ($document->approved == 1) ? "received-yes uk-float-left" : "check-received-no received-no" }}"></span>
+									<span style="float: left;margin-top:8px;margin-left: 5px" id="sent-id-{{ $document->id }}category-id-1-not-received-icon" class="{{ ($document->notapproved == 1) ? "a-circle-cross alert" : "a-checkbox" }} {{ ($document->approved == 1) ? " minus-received-yes received-yes" : "received-no" }}"></span>
+									<span style="display: block; margin-left: 55px;margin-top:2px">{{ $document_category->parent->document_category_name }} : {{ $document_category->document_category_name }} </span>
+								</a>
+								<div uk-dropdown="mode: click" id="#sent-id-{{ $document->id }}-category-id-{{ $document_category->id }}">
+									<ul class="uk-nav uk-nav-dropdown">
 
-		    				<li class="{{ ($document->notapproved == 1) ? "declined-category s" : "" }} {{ ($document->approved == 1) ? "approved-category" : "" }}">
-		    					<a id="sent-id-{{ $document->id }}-category-id-{{ $document_category->id }}" class="">
-		    						<span style="float: left;margin-top:8px;margin-left: 5px"  id="sent-id-{{ $document->id }}-category-id-1-recieved-icon" class="a-checkbox-checked {{ ($document->approved == 1) ? "received-yes uk-float-left" : "check-received-no received-no" }}"></span>
-		    						<span style="float: left;margin-top:8px;margin-left: 5px" id="sent-id-{{ $document->id }}category-id-1-not-received-icon" class="{{ ($document->notapproved == 1) ? "a-circle-cross alert" : "a-checkbox" }} {{ ($document->approved == 1) ? " minus-received-yes received-yes" : "received-no" }}"></span>
-		    						<span style="display: block; margin-left: 55px;margin-top:2px">{{ $document_category->parent->document_category_name }} : {{ $document_category->document_category_name }} </span>
-		    					</a>
-		    					@if($document->comment) <div style="display: block; margin-left:35.25px;"><i class="a-comment"></i> "{{ $document->comment }}" </div>@endif
+										<li>
+											<a onclick="markApproved({{ $document->id }},{{ $document_category->id }});">
+												Mark as approved
+											</a>
+										</li>
+										<li>
+											<a onclick="markNotApproved({{ $document->id }},{{ $document_category->id }});">
+												Mark as declined
+											</a>
+										</li>
+										<li>
+											<a onclick="markUnreviewed({{ $document->id }},{{ $document_category->id }});">
+												Clear review status
+											</a>
+										</li>
+									</ul>
+								</div>
+								@if($document->comment) <div style="display: block; margin-left:35.25px;"><i class="a-comment"></i> "{{ $document->comment }}" </div>@endif
+							</li>
+							@endforeach
+						</ul>
+					</td>
+					<td style="padding-left: 10px">
+						BUILDINGS {{ count($buildings) }} | UNITS {{ count($units) }}
+					</td>
+					<td style="padding-left: 10px">
+						@if(count($all_ids) > 0)
+						<span  >@if(count($all_ids) > 1) Audits: {{ implode(', ',$all_ids) }} @elseIf(count($all_ids)) Audit: {{ implode(', ',$all_ids) }} @endIf</span> |
+						<span uk-tooltip="pos: right" title="@if(count($document_findings) > 0){{ implode(', ', $document_findings->pluck('id')->toArray()) }}@endif">
+							<span onclick="$('#document-{{ $document->id }}-findings').slideToggle();" class="use-hand-cursor" uk-tooltip title="CLICK TO VIEW FINDING(S)">
+								Total Findings: <span class="uk-badge finding-number {{ $unresolved_findings > 0 ? 'attention' : '' }} " uk-tooltip="" title="" aria-expanded="false"> {{ @count($document_findings) }}</span>
+							</span>
+						</span>
 
-		    				</li>
-		    				{{-- @endif --}}
-		    				@endforeach
-		    			</ul>
-		    		</td>
-
-		    		<td style="padding-left: 10px">
-
-		    			@if($building_findings != NULL && count($building_findings))
-		    			BUILDINGS {{ count($building_findings) }}
-		    			@else
-		    			BUILDINGS 0
-		    			@endIf
-
-		    			@if($unit_findings != NULL && count($unit_findings))
-		    			| UNITS {{ count($unit_findings) }}
-		    			@else
-		    			| UNITS 0
-		    			@endIf
-
-
-
-		    		</td>
-		    		<td style="padding-left: 10px">
-		    			<?php $unitAudits = $document_audits->pluck('id')->toArray();?>
-		    			@if(count($unitAudits))
-		    			<span  >@if(count($unitAudits) > 1) Audits: {{ implode(', ',$unitAudits) }} @elseIf(count($unitAudits)) Audit:{{ implode(', ',$unitAudits) }} @endIf</span> |
-		    			<span uk-tooltip="pos: right" title="@if($document->has_findings){{ implode(', ', $document_findings->pluck('id')->toArray()) }}@endif">
-		    				<span onclick="$('#document-{{ $document->id }}-findings').slideToggle();" class="use-hand-cursor" uk-tooltip title="CLICK TO VIEW FINDING(S)">
-		    					Total Findings: <span class="uk-badge finding-number {{ $unresolved_findings > 0 ? 'attention' : '' }} " uk-tooltip="" title="" aria-expanded="false"> {{ @count($document_findings) }}</span>
-		    				</span>
-		    			</span>
-
-		    			<div id="document-{{ $document->id }}-findings" style="display: none;">
-		    				{{-- @foreach($document_findings as $fin) --}}
-		    				<hr class="uk-margin-bottom" style="border: 1px solid #bbbbbb" />
-		    				<li>
-		    					@include('non_modal.finding-summary')
-		    				</li>
-		    				{{-- @endforeach --}}
-		    			</div>
-		    			@else
-		    			NA | NA
-		    			@endIf
-		    		</td>
-
-
-
-		    		<td>
-		    			<a class="uk-link-muted " uk-tooltip="@foreach($document->assigned_categories as $document_category){{ $document_category->parent->document_category_name }}/{{ $document_category->document_category_name }}@endforeach">
-		    				<span class="a-info-circle"  style="color: #56b285;"></span>
-		    			</a>
-		    			&nbsp;|&nbsp;
-		    			<a class="uk-link-muted " onclick="dynamicModalLoad('edit-local-document/{{ $document->id }}')" uk-tooltip="Edit this file">
-		    				<span class="a-pencil-2" style="color: rgb(0, 193, 247);"></span>
-		    			</a>
-		    			&nbsp;|&nbsp;
-		    			@if($admin_access)
-		    			<a class="uk-link-muted " onclick="deleteFile({{ $document->id }});" uk-tooltip="Delete this file">
-		    				<span class="a-trash-4" style="color: #da328a;"></span>
-		    			</a>
-		    			&nbsp;|&nbsp;
-		    			@endif
-		    			<a href="download-local-document/{{ $document->id }}" target="_blank"  uk-tooltip="Download file." download>
-		    				<span class="a-lower"></span>
-		    			</a>
-
-		    		</td>
-		    	</span>
-		    </span>
-		  </tr>
-		  @endforeach
-		</tbody>
-	</table>
-	<div id="documents-tab-pages-and-filters-2">
-		{{ $documents->links() }}
+						<div id="document-{{ $document->id }}-findings" style="display: none;">
+							{{-- @foreach($document_findings as $fin) --}}
+							<hr class="uk-margin-bottom" style="border: 1px solid #bbbbbb" />
+							<li>
+								@include('non_modal.finding-summary')
+							</li>
+							{{-- @endforeach --}}
+						</div>
+						@else
+						NA | NA
+						@endIf
+					</td>
+					<td>
+						<a class="uk-link-muted " uk-tooltip="@foreach($document->assigned_categories as $document_category){{ $document_category->parent->document_category_name }}/{{ $document_category->document_category_name }}@endforeach">
+							<span class="a-info-circle"  style="color: #56b285;"></span>
+						</a>
+						&nbsp;|&nbsp;
+						<a class="uk-link-muted " onclick="dynamicModalLoad('edit-local-document/{{ $document->id }}')" uk-tooltip="Edit this file">
+							<span class="a-pencil-2" style="color: rgb(0, 193, 247);"></span>
+						</a>
+						&nbsp;|&nbsp;
+						@if($admin_access)
+						<a class="uk-link-muted " onclick="deleteFile({{ $document->id }});" uk-tooltip="Delete this file">
+							<span class="a-trash-4" style="color: #da328a;"></span>
+						</a>
+						&nbsp;|&nbsp;
+						@endif
+						<a href="download-local-document/{{ $document->id }}" target="_blank"  uk-tooltip="Download file." download>
+							<span class="a-lower"></span>
+						</a>
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+		<div id="documents-tab-pages-and-filters-2">
+			{{ $documents->links() }}
+		</div>
 	</div>
-</div>
+
 </div>
 
 
@@ -361,10 +370,10 @@
 
 	$(document).ready(function(){
 		var tempdiv = '<div style="height:100px;text-align:center;"><div uk-spinner style="margin: 20px 0;"></div></div>';
-
 		$('#documents-tab-pages-and-filters .page-link').click(function(){
 			$('#local-documents').html(tempdiv);
 			$('#allita-documents').load($(this).attr('href'));
+			window.currentDocumentsPage = $(this).attr('href');
 			return false;
 		});
 
@@ -373,8 +382,6 @@
 			$('#allita-documents').load($(this).attr('href'));
 			return false;
 		});
-
-
 		$('#local-documents-search').keydown(function (e) {
 			if (e.keyCode == 13) {
 				searchDocuments();
@@ -391,7 +398,6 @@
 		var reviewed = 'off';
 		var unreviewed = 'off';
 		$('#local-documents').html(tempdiv);
-
 		if($("#documents-unreviewed-checkbox").prop("checked") == true){
 			unreviewed = "on";
 		}
@@ -404,7 +410,7 @@
 		if($("#documents-findings-corrected-checkbox").prop("checked") == true){
 			resolved = "on";
 		}
-		$.post('{{ URL::route("documents.local-search", $project->id) }}', {
+		$.post('{{ URL::route("pm-documents.local-search", $project->id) }}', {
 			'local-unresolved' : unresolved,
 			'local-resolved' : resolved,
 			'local-reviewed' : reviewed,
@@ -422,16 +428,7 @@
 		} );
 	}
 
-    // process search
-    $(document).ready(function() {
-    	$('#users-search').keydown(function (e) {
-    		if (e.keyCode == 13) {
-    			searchUsers();
-    			e.preventDefault();
-    			return false;
-    		}
-    	});
-    });
+  
 
     function filterByAudit(){
     	console.log('ada');
@@ -458,80 +455,11 @@
 	// 	console.log(filters);
 	// }
 
-	function getSelectedFilters() {
-		auditId = $('#document-filter-by-audit :selected').val();
-		findingId = $('#document-filter-by-finding :selected').val();
-		categoryId = $('#document-filter-by-category :selected').val();
-		unitId = $('#document-filter-by-unit :selected').val();
-
-		documentName = $('#documents-name-search').val();
-		// debugger;
-		$(".all").hide();
-
-		// return;
-		cssFilter = "";
-		filter = '?';
-		if(categoryId != "") {
-			//filter = filter + '&filter_category_id='+categoryId;
-
-			//cssFilter = cssFilter+' .' +categoryId;
-			$("."+categoryId).show();
-		}
-		if(auditId != "") {
-			filter = filter + '&filter_audit_id='+auditId;
-
-			//hide other audits
-			$('li[class*=audit-]').hide();
-
-			//cssFilter = '.' +auditId;
-			$("."+auditId).show();
-
-		}
-		if(unitId != "") {
-			filter = filter + '&filter_unit_id='+unitId;
-			$('li[class*=unit-]').hide();
-			//if(cssFilter.length) { cssFilter = cssFilter+" , "; }
-			//cssFilter = cssFilter+' .' +unitId;
-
-			if(findingId != "" && findingId == 'finding-resolved'){
-				$("."+unitId).show();
-				$(".finding-unresolved").hide();
-			} else if(findingId != "" && findingId == 'finding-unresolved'){
-				$("."+unitId).show();
-				$(".finding-resolved").hide();
-			} else if(findingId != "") {
-				$("."+findingId+", ."+unitId).show();
-			} else {
-				$("."+unitId).show();
-			}
-			console.log('showing unit '+unitId);
-		}
-		if(findingId != "" && unitId == "") {
-			filter = filter + '&filter_finding_id='+findingId;
-			//if(auditId.length) { cssFilter = cssFilter+" , "; }
-			//cssFilter = cssFilter+' .' +findingId;
-			$('li[class*=finding-]').hide();
-			$("."+findingId).show();
-		}
-
-		// if(cssFilter.length) {
-		// 	$(cssFilter).show();
-		// 	console.log('Showing '+cssFilter);
-		// }
-		if(auditId == "" && findingId == "" && categoryId == "" && unitId== "") {
-			$(".all").show();
-			console.log('showing all');
-
-		}
-
-		// documentsLocal("{{ $project->id }}", "{{ $audit_id }}", filter);
-		// return filter = [auditId, findingId, categoryId, documentName];
-	}
 
 	function openFindingDetails(findingId) {
 		dynamicModalLoad('finding-details/'+findingId);
 	}
 
-
+	
 
 </script>
